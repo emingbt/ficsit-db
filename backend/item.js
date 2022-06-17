f6b7950cbcdad639ad4ad3c49652446295aa7644
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-06-17 19:28:32 +0300

    Add item module

---
+++ backend/item.js
@@ -0,0 +1,10 @@
+class Item { 
+  constructor(name, description, image, details, recipes, usagesAsIngredient, usagesForBuildings) {
+    this.name = name
+    this.description = description
+    this.image = image
+    this.details = details
+  }
+}
+
+module.exports = Item
