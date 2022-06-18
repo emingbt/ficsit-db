c250997b834be3013b2fa2ae9c0c5c49e025637b
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-06-18 15:41:26 +0300

    Display an item

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+const Item = require('../models/item')
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'FICSIT DB' })
+})
+
+const item = new Item(
+  "Copper Ore",
+  "Ore, just ore",
+  "images/blablabla.png",
+  {
+    sinkPoints: 3,
+    stackSize: 100,
+    radioactive: false
+  }
+)
+
+/* GET one item. */
+router.get('/items', (req, res, next) => {
+  res.render('item', { item })
 })
 
 module.exports = router
