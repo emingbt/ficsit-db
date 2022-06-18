0f1158f7ea7f096725024b4de4e24bddf2e7e7f9
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-06-18 16:28:37 +0300

    Implement database connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
+require('./database-connection')
+
 const indexRouter = require('./routes/index')
 const itemsRouter = require('./routes/items')
 
