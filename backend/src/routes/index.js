5ee9e2182cd2bbe8c4de0676fbfae40d453f201f
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-06-20 18:02:39 +0300

    Test models on items route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,20 +8,4 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'FICSIT DB' })
 })
 
-const item = new Item(
-  "Copper Ore",
-  "Ore, just ore",
-  "images/blablabla.png",
-  {
-    sinkPoints: 3,
-    stackSize: 100,
-    radioactive: false
-  }
-)
-
-/* GET one item. */
-router.get('/items', (req, res, next) => {
-  res.render('item', { item })
-})
-
 module.exports = router
