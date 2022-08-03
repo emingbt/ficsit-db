9c423ba34cc35e8758d7d5ae83187df7e9956d9e
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-08-03 12:06:40 +0300

    Update recipe component

--- frontend/components/sections/recipe.js
+++ frontend/components/sections/recipe.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import styled from "styled-components"
 
 export default function Recipe() {
@@ -94,25 +95,44 @@ export default function Recipe() {
       <StyledRecipeContainer>
         {mockRecipeData.map((e) => {
           return (
-            <StyledRecipe>
-              <StyledContainer>
+            <StyledRecipe key={e.slug}>
+              <StyledContainer flexDirection="row">
                 {e.ingredients.map((i) => {
                   return (
-                    <StyledIngredient>
-                      <p>{i.quantity}x</p>
-                      <div>
-                        <div>{i.itemClass}</div>
-                        <p>{60 / e.craftTime * i.quantity}/min</p>
-                      </div>
-                    </StyledIngredient>
+                    <StyledItem key={i.itemClass}>
+                      <StyledItemQuantity>{i.quantity}x</StyledItemQuantity>
+                      <StyledContainer flexDirection="column">
+                        <Link href={`/items/${i.itemClass}`}>
+                          <StyledItemImage name={i.itemClass} />
+                        </Link>
+                        <StyledUsePerMin>{60 / e.craftTime * i.quantity}/min</StyledUsePerMin>
+                      </StyledContainer>
+                    </StyledItem>
                   )
                 })}
               </StyledContainer>
               <StyledContainer>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <StyledVerticalLine isAlternate={e.isAlternate} />
+                <StyledContainer flexDirection="row">
+                  {e.products.map((j) => {
+                    return (
+                      <StyledItem product key={j.itemClass}>
+                        <StyledItemQuantity>{j.quantity}x</StyledItemQuantity>
+                        <StyledContainer flexDirection="column" style={{maxWidth: '80px'}}>
+                          <Link href={`/items/${j.itemClass}`}>
+                            <StyledItemImage name={j.itemClass} />
+                          </Link>
+                          <StyledUsePerMin>{60 / e.craftTime * j.quantity}/min</StyledUsePerMin>
+                        </StyledContainer>
+                      </StyledItem>
+                    )
+                  })}
+                </StyledContainer>
+                <StyledVerticalLine isAlternate={e.isAlternate} />
+                <StyledBuildingContainer>
+                  <StyledBuildingImage producedIn={e.producedIn} />
+                  <StyledCraftTime>{e.craftTime}sn</StyledCraftTime>
+                </StyledBuildingContainer>
               </StyledContainer>
             </StyledRecipe>
           )
@@ -161,14 +181,75 @@ const StyledRecipe = styled.div`
 `
 
 const StyledContainer = styled.div`
+  height: 100%;
   display: flex;
-  flex-direction: row;
+  flex-direction: ${props => props.flexDirection};
   justify-content: center;
+  align-items: center;
 `
 
-const StyledIngredient = styled.div`
+const StyledItem = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-around;
   padding: 0 1rem;
+  min-width: 180px;
+  margin-top: 1rem;
+  ${props => props.product && 'margin-right: 2rem;'}
+`
+
+const StyledItemQuantity = styled.div`
+  font-size: 2rem;
+  margin-top: 1rem;
+`
+
+const StyledItemImage = styled.div`
+  height: 80px;
+  aspect-ratio: 1 / 1;
+  background-color: #202125;
+  border-radius: 8px;
+  background-image: url(${props => 'https://u6.satisfactorytools.com/assets/images/items/' + props.name + '_256.png'});
+  background-size: 90%;
+  background-position: center;
+  background-repeat: no-repeat;
+  cursor: pointer;
+`
+
+const StyledUsePerMin = styled.p`
+  font-size: 1.25rem;
+  margin: 0.5rem;
+`
+
+const StyledVerticalLine = styled.div`
+  height: 100%;
+  width: 3px;
+  background-color: #D79845;
+  ${props => props.isAlternate && 'background: repeating-linear-gradient(-35deg,#E5AF07,#E5AF07 10px,#202125 10px,#202125 20px)'}
+`
+
+const StyledBuildingContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 150px;
+  margin-top: 1rem;
+`
+
+const StyledBuildingImage = styled.div`
+  height: 80px;
+  aspect-ratio: 1 / 1;
+  background-color: #202125;
+  border-radius: 8px;
+  background-image: url(${props => 'https://u6.satisfactorytools.com/assets/images/items/' + props.producedIn + '_256.png'});
+  background-size: 90%;
+  background-position: center;
+  background-repeat: no-repeat;
+  cursor: pointer;
+`
+
+const StyledCraftTime = styled.div`
+  font-size: 1.25rem;
+  margin: 0.5rem;
 `
