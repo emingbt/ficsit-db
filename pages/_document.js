77246507b6a24920aa589bb234e623a6c946840c
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-09-08 16:19:47 +0300

    Give logo font to head in document

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,26 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/INDUSTRIAL-BRANDING.ttf"
+            as="font"
+            type="font/ttf"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
