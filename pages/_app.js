fb7616637a835b8bf691f691fbfd0b5ce5893f6c
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-09-08 16:37:08 +0300

    Change font display with optional

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: Industrial Branding;
     src: url('/fonts/INDUSTRIAL-BRANDING.ttf') format('truetype');
-    font-display: swap;
+    font-display: optional;
   }
 
   @font-face {
