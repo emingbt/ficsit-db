6429f7441fb0a5db795459f041f4959e6d180536
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-09-12 13:52:41 +0300

    Add priority to banner image in home page

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
               layout="fill"
               objectFit='cover'
               alt="Banner"
+              priority
             />
           </StyledBannerContainer>
           <StyledBannerText>
