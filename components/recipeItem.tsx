da8b46aa479c7d95c0c3ae79fd90a678cb194394
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-03-22 13:04:15 +0300

    Split recipe page to components

---
+++ components/recipeItem.tsx
@@ -0,0 +1,129 @@
+import styled from "styled-components"
+import Image from "next/image"
+import Link from "next/link"
+
+import { IsFluid, Primary } from "../interfaces/styledComponents"
+import { ProductionRecipeItem } from "../interfaces"
+
+interface Props {
+  craftTime: number,
+  item: ProductionRecipeItem,
+  clockspeed: number
+}
+
+export default function RecipeItem({ craftTime, item, clockspeed }: Props) {
+  return (
+    <StyledItemContainer key={item.itemClass}>
+      <StyledItem>
+        <Link href={`/items/${item.itemClass}`}>
+          <StyledItemImage isFluid={item.isFluid}>
+            <Image
+              src={`/images/items/${item.itemClass}.png`}
+              width={72}
+              height={72}
+              alt={item.itemClass}
+              quality={100}
+            />
+          </StyledItemImage>
+        </Link>
+        <StyledItemDetail>
+          <StyledItemName>{item.quantity} {item.name}</StyledItemName>
+          <StyledUsagePerMinute>
+            <span style={{ color: '#e59344' }}>{parseFloat((60 / craftTime * item.quantity).toFixed(3))} </span>
+            {clockspeed != 100 && (
+              <strong style={{ color: '#e59344' }}>({parseFloat((60 / craftTime * item.quantity * clockspeed / 100).toFixed(4))})</strong>
+            )} per minute</StyledUsagePerMinute>
+        </StyledItemDetail>
+      </StyledItem>
+      <StyledItemLine />
+    </StyledItemContainer>
+  )
+}
+
+const StyledItemLine = styled.div<Primary>`
+  width: ${props => props.primary ? '96%' : '100%'};
+  height: 1px;
+  background-color: #9BA3A9;
+`
+
+const StyledItemContainer = styled.div`
+  width: 96%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const StyledItem = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`
+
+const StyledItemImage = styled.div<IsFluid>`
+  height: 2.5rem;
+  width: 2.5rem;
+  background-color: #43454B;
+  padding: 0.25rem;
+  margin: 0.5rem 0.25rem 0.5rem 0;
+  border-radius: ${props => props.isFluid ? '50%' : '0.125rem'};
+  ${props => props.detail && 'margin: 0 0 0 0.25rem;'}
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: 3rem;
+    height: 3rem;
+  }
+
+  @media (min-width: 1440px) {
+    width: 4.5rem;
+    height: 4.5rem;
+    margin: 0.5rem 0.5rem 0.5rem 0;
+  }
+`
+
+const StyledItemDetail = styled.div`
+  width: calc(100% - 3rem);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  color: #CCCCCC;
+  font-family: Roboto, -apple-system, sans-serif;
+
+  @media (min-width: 1024px) {
+    width: calc(100% - 4rem);
+  }
+
+  @media (min-width: 1440px) {
+    width: calc(100% - 5rem);
+  }
+`
+
+const StyledItemName = styled.div`
+  color: #DDDDDD;
+  font-size: 0.5rem;
+  margin-bottom: 0.125rem;
+
+  @media (min-width: 768px) {
+    font-size: 0.75rem;
+  }
+
+  @media (min-width: 1440px) {
+    font-size: 1rem;
+  }
+`
+
+const StyledUsagePerMinute = styled.div`
+  font-size: 0.4rem;
+
+  @media (min-width: 768px) {
+    font-size: 0.6rem;
+  }
+
+  @media (min-width: 1440px) {
+    font-size: 0.8rem;
+  }
+`
--- pages/recipes/[name].tsx
+++ pages/recipes/[name].tsx
@@ -2,13 +2,12 @@ import React, { useState } from 'react'
 import { getBuildableByName, getRecipeByRecipeName } from '../api'
 import styled from 'styled-components'
 import { Container, Main, StyledTitle, StyledLine } from '../../components/sharedstyles'
-import Image from 'next/image'
-import Link from 'next/link'
 import Head from 'next/head'
 import RecipeDetails from '../../components/sections/recipeDetails'
+import RecipeItem from '../../components/recipeItem'
 
 import { ProductionRecipe, Building } from '../../interfaces'
-import { Primary, IsFluid } from '../../interfaces/styledComponents'
+import { Primary } from '../../interfaces/styledComponents'
 
 interface Props {
   recipe: ProductionRecipe,
@@ -42,30 +41,11 @@ export default function Recipe({ recipe, building }: Props) {
                   <StyledItemLine primary />
                   {recipe.ingredients.map((ingredient) => {
                     return (
-                      <StyledItemContainer key={ingredient.itemClass}>
-                        <StyledItem>
-                          <Link href={`/items/${ingredient.itemClass}`}>
-                            <StyledItemImage isFluid={ingredient.isFluid}>
-                              <Image
-                                src={`/images/items/${ingredient.itemClass}.png`}
-                                width={72}
-                                height={72}
-                                alt={ingredient.itemClass}
-                                quality={100}
-                              />
-                            </StyledItemImage>
-                          </Link>
-                          <StyledItemDetail>
-                            <StyledItemName>{ingredient.quantity} {ingredient.name}</StyledItemName>
-                            <StyledUsagePerMinute>
-                              <span style={{ color: '#e59344' }}>{parseFloat((60 / recipe.craftTime * ingredient.quantity).toFixed(3))} </span>
-                              {clockspeed != 100 && (
-                                <strong style={{ color: '#e59344' }}>({parseFloat((60 / recipe.craftTime * ingredient.quantity * clockspeed / 100).toFixed(4))})</strong>
-                              )} per minute</StyledUsagePerMinute>
-                          </StyledItemDetail>
-                        </StyledItem>
-                        <StyledItemLine />
-                      </StyledItemContainer>
+                      <RecipeItem
+                        craftTime={recipe.craftTime}
+                        item={ingredient}
+                        clockspeed={clockspeed}
+                      />
                     )
                   })}
                 </StyledItemsList>
@@ -85,30 +65,11 @@ export default function Recipe({ recipe, building }: Props) {
                   <StyledItemLine primary />
                   {recipe.products.map((product) => {
                     return (
-                      <StyledItemContainer key={product.itemClass}>
-                        <StyledItem>
-                          <Link href={`/items/${product.itemClass}`}>
-                            <StyledItemImage isFluid={product.isFluid}>
-                              <Image
-                                src={`/images/items/${product.itemClass}.png`}
-                                width={72}
-                                height={72}
-                                alt={product.itemClass}
-                                quality={100}
-                              />
-                            </StyledItemImage>
-                          </Link>
-                          <StyledItemDetail>
-                            <StyledItemName>{product.quantity} {product.name}</StyledItemName>
-                            <StyledUsagePerMinute>
-                              <span style={{ color: '#e59344' }}>{parseFloat((60 / recipe.craftTime * product.quantity).toFixed(3))} </span>
-                              {clockspeed != 100 && (
-                                <strong style={{ color: '#e59344' }}>({parseFloat((60 / recipe.craftTime * product.quantity * clockspeed / 100).toFixed(4))})</strong>
-                              )} per minute</StyledUsagePerMinute>
-                          </StyledItemDetail>
-                        </StyledItem>
-                        <StyledItemLine />
-                      </StyledItemContainer>
+                      <RecipeItem
+                        craftTime={recipe.craftTime}
+                        item={product}
+                        clockspeed={clockspeed}
+                      />
                     )
                   })}
                 </StyledItemsList>
@@ -254,86 +215,4 @@ const StyledItemLine = styled.div<Primary>`
   width: ${props => props.primary ? '96%' : '100%'};
   height: 1px;
   background-color: #9BA3A9;
-`
-
-const StyledItemContainer = styled.div`
-  width: 96%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
-const StyledItem = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-`
-
-const StyledItemImage = styled.div<IsFluid>`
-  height: 2.5rem;
-  width: 2.5rem;
-  background-color: #43454B;
-  padding: 0.25rem;
-  margin: 0.5rem 0.25rem 0.5rem 0;
-  border-radius: ${props => props.isFluid ? '50%' : '0.125rem'};
-  ${props => props.detail && 'margin: 0 0 0 0.25rem;'}
-  cursor: pointer;
-
-  @media (min-width: 768px) {
-    width: 3rem;
-    height: 3rem;
-  }
-
-  @media (min-width: 1440px) {
-    width: 4.5rem;
-    height: 4.5rem;
-    margin: 0.5rem 0.5rem 0.5rem 0;
-  }
-`
-
-const StyledItemDetail = styled.div`
-  width: calc(100% - 3rem);
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  color: #CCCCCC;
-  font-family: Roboto, -apple-system, sans-serif;
-
-  @media (min-width: 1024px) {
-    width: calc(100% - 4rem);
-  }
-
-  @media (min-width: 1440px) {
-    width: calc(100% - 5rem);
-  }
-`
-
-const StyledItemName = styled.div`
-  color: #DDDDDD;
-  font-size: 0.5rem;
-  margin-bottom: 0.125rem;
-
-  @media (min-width: 768px) {
-    font-size: 0.75rem;
-  }
-
-  @media (min-width: 1440px) {
-    font-size: 1rem;
-  }
-`
-
-const StyledUsagePerMinute = styled.div`
-  font-size: 0.4rem;
-
-  @media (min-width: 768px) {
-    font-size: 0.6rem;
-  }
-
-  @media (min-width: 1440px) {
-    font-size: 0.8rem;
-  }
 `
