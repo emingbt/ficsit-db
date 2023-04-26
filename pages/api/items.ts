50d09520b5bbb140a907c64be437e6bbbe187327
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-04-27 00:00:33 +0300

    Add /items handler to api folder and update fetching method in items page

---
+++ pages/api/items.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import executeQuery from "../../utils/neo4j";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const query = `
+    MATCH (i:Item)
+    RETURN i.slug AS slug, i.name AS name, i.imgUrl AS imgUrl
+    ORDER BY i.slug
+  `;
+
+  const result = await executeQuery(query);
+
+  res.status(200).json(result);
+}
