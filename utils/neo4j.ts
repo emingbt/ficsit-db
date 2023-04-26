890b0f020906b2c5c7547edd7b9aae1186296484
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-04-26 23:56:03 +0300

    Update neo4j file in utils folder

--- utils/neo4j.ts
+++ utils/neo4j.ts
@@ -1,48 +1,25 @@
 import neo4j, { Driver, Session } from 'neo4j-driver'
 
 const driver: Driver = neo4j.driver(
-  // Connect to the Neo4j instance
   process.env.NEO4J_URI,
   neo4j.auth.basic(
-    process.env.NEO4J_USERNAME,
+    process.env.NEO4J_USER,
     process.env.NEO4J_PASSWORD
   )
 )
 
-export async function read(cypher: string, params = {}) {
-  // 1. Open a session
-  const session: Session = driver.session()
-
+const executeQuery = async (query: string, params?: {}) => {
   try {
-    // 2. Execute a Cypher Statement
-    const res = await session.executeRead(tx => tx.run(cypher, params))
+    const session: Session = driver.session({ database: 'neo4j' })
+    const result = await session.executeRead(tx => tx.run(query, params))
 
-    // 3. Process the Results
-    const values = res.records.map(record => record.toObject())
+    console.log(result.records.map(record => record.toObject()))
 
-    return values
-  }
-  finally {
-    // 4. Close the session 
     await session.close()
+    return result.records.map(record => record.toObject())
+  } catch (error) {
+    console.log(error)
   }
 }
 
-export async function write(cypher: string, params = {}) {
-  // 1. Open a session
-  const session = driver.session()
-
-  try {
-    // 2. Execute a Cypher Statement
-    const res = await session.executeWrite(tx => tx.run(cypher, params))
-
-    // 3. Process the Results
-    const values = res.records.map(record => record.toObject())
-
-    return values
-  }
-  finally {
-    // 4. Close the session 
-    await session.close()
-  }
-}
+export default executeQuery
