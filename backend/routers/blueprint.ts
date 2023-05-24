c8454a94a23b173fbde60ee597fd49b791c3c8b0
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-05-25 00:43:19 +0300

    Update blueprint router file

--- backend/routers/blueprint.ts
+++ backend/routers/blueprint.ts
@@ -2,7 +2,10 @@ import { publicProcedure, router } from "../utils/trpc"
 import z from 'zod'
 import {
   getAllBlueprints,
-  getBlueprintById
+  getBlueprintById,
+  createBlueprint,
+  updateBlueprint,
+  deleteBlueprint
 } from "../services/blueprint"
 
 export const blueprintRouter = router({
@@ -16,4 +19,36 @@ export const blueprintRouter = router({
       const blueprint = await getBlueprintById(input)
       return blueprint
     }),
+  createBlueprint: publicProcedure
+    .input(z.object({
+      title: z.string(),
+      description: z.string(),
+      fileLinks: z.array(z.string()),
+      imageLinks: z.array(z.string()),
+      categories: z.array(z.string()),
+      designerId: z.number()
+    }))
+    .mutation(async ({ input }) => {
+      const createdBlueprint = await createBlueprint(input)
+      return createdBlueprint
+    }),
+  updateBlueprint: publicProcedure
+    .input(z.object({
+      id: z.number(),
+      title: z.string(),
+      description: z.string(),
+      fileLinks: z.array(z.string()),
+      imageLinks: z.array(z.string()),
+      categories: z.array(z.string()),
+    }))
+    .mutation(async ({ input }) => {
+      const updatedBlueprint = await updateBlueprint(input)
+      return updatedBlueprint
+    }),
+  deleteBlueprint: publicProcedure
+    .input(z.object({ blueprintId: z.number() }))
+    .mutation(async ({ input }) => {
+      const deletedBlueprint = await deleteBlueprint(input)
+      return deletedBlueprint
+    })
 })
