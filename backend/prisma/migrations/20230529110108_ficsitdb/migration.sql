7954f31fe7492b6b338c96104e30334d210e2f09
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-05-31 13:12:33 +0300

    Update prisma client's binaryTargets

--- backend/prisma/migrations/20230529110108_ficsitdb/migration.sql
+++ backend/prisma/migrations/20230529110108_ficsitdb/migration.sql
@@ -1,40 +1,40 @@
 -- CreateTable
 CREATE TABLE "Upvote" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "blueprintId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "blueprintId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Upvote_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Comment" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "content" TEXT,
-    "authorId" INTEGER NOT NULL,
-    "blueprintId" INTEGER NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "blueprintId" TEXT NOT NULL,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Blueprint" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
     "fileLinks" TEXT[],
     "imageLinks" TEXT[],
     "categories" TEXT[],
-    "designerId" INTEGER NOT NULL,
+    "designerId" TEXT NOT NULL,
 
     CONSTRAINT "Blueprint_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
@@ -47,6 +47,9 @@ CREATE UNIQUE INDEX "Blueprint_designerId_key" ON "Blueprint"("designerId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "Upvote" ADD CONSTRAINT "Upvote_blueprintId_fkey" FOREIGN KEY ("blueprintId") REFERENCES "Blueprint"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
