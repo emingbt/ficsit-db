d3a72dea74c6d5b6463b4592e3c7f28d5762e6e4
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-06-04 20:27:15 +0300

    Update user schema

--- backend/prisma/migrations/20230602102012_ficsitdb/migration.sql
+++ backend/prisma/migrations/20230602102012_ficsitdb/migration.sql
@@ -37,7 +37,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "memberSince" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
