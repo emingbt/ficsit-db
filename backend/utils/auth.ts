3fbfad5137aa4894c100c15f1a40b4e36681e29b
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-06-04 20:28:03 +0300

    Update authentication functions

--- backend/utils/auth.ts
+++ backend/utils/auth.ts
@@ -1,34 +1,59 @@
 import jwt from 'jsonwebtoken'
+import { getUserById } from '../services/user'
+import { TRPCError } from '@trpc/server'
 
-export const generateToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET!, {
-    algorithm: 'HS256',
-    expiresIn: '30d'
+// Token types enum
+export enum TokenType {
+  access = 'ACCESS',
+  resetPassword = 'RESET_PASSWORD',
+  emailConfirmation = 'EMAIL_CONFIRMATION'
+}
+
+// Token expiration times
+const expirationTimes = {
+  [TokenType.access]: '30d',
+  [TokenType.resetPassword]: '30m',
+  [TokenType.emailConfirmation]: '30m'
+}
+
+// Generate token
+export const generateToken = (id: string, tokenType = TokenType.access) => {
+  // Get secret from environment variables
+  const secret = process.env[`JWT_${tokenType}_SECRET`]
+
+  // If secret is not defined, throw error
+  if (!secret) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'JWT secret is not defined'
+    })
+  }
+
+  // Create and return token
+  return jwt.sign({ id }, secret!, {
+    expiresIn: expirationTimes[tokenType]
   })
 }
 
-export const verifyToken = (token: string) => {
-  return jwt.verify(token, process.env.JWT_SECRET!)
-}
+// Verify token
+export const verifyToken = async (token: string, tokenType = TokenType.access) => {
+  try {
+    const secret = process.env[`JWT_${tokenType}_SECRET`]
+    const decoded = jwt.verify(token, secret!)
+    const { id } = decoded as { id: string }
 
-export const generateResetPasswordToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_RESET_PASSWORD_SECRET!, {
-    algorithm: 'HS256',
-    expiresIn: '30m'
-  })
-}
+    if (!id) {
+      return false
+    }
 
-export const verifyResetPasswordToken = (token: string) => {
-  return jwt.verify(token, process.env.JWT_RESET_PASSWORD_SECRET!)
-}
+    const user = await getUserById({ id })
 
-export const generateEmailConfirmationToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_EMAIL_CONFIRMATION_SECRET!, {
-    algorithm: 'HS256',
-    expiresIn: '30m'
-  })
-}
+    if (!user) {
+      return false
+    }
 
-export const verifyEmailConfirmationToken = (token: string) => {
-  return jwt.verify(token, process.env.JWT_EMAIL_CONFIRMATION_SECRET!)
+    return user
+  } catch (err) {
+    throw new TRPCError({ code: 'BAD_REQUEST', message: 'Invalid token' })
+  }
 }
