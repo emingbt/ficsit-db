3b52eb2bf56f077c1ef83c0aed0879381c5488ff
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-06-20 13:23:40 +0300

    Instead of giving blueprint id use blueprint title on router

--- backend/routers/upvote.ts
+++ backend/routers/upvote.ts
@@ -4,13 +4,13 @@ import { upvoteBlueprint, removeUpvote } from "../services/upvote"
 
 export const upvoteRouter = router({
   upvoteBlueprint: protectedProcedure
-    .input(z.object({ blueprintId: z.string() }))
+    .input(z.object({ blueprintTitle: z.string() }))
     .mutation(async ({ input, ctx }) => {
       const upvote = await upvoteBlueprint({ input, ctx })
       return upvote
     }),
   removeUpvote: protectedProcedure
-    .input(z.object({ blueprintId: z.string() }))
+    .input(z.object({ blueprintTitle: z.string() }))
     .mutation(async ({ input, ctx }) => {
       const removedUpvote = await removeUpvote({ input, ctx })
       return removedUpvote
