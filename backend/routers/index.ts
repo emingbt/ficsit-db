e5c0b912da7bd8ea71df7ca8116106bbe055c84f
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-06-20 15:03:15 +0300

    Add comment router to the app router

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -1,10 +1,12 @@
 import { router } from "../utils/trpc"
 import { blueprintRouter } from "./blueprint"
+import { commentRouter } from "./comment"
 import { upvoteRouter } from "./upvote"
 import { userRouter } from "./user"
 
 export const appRouter = router({
   blueprint: blueprintRouter,
+  comment: commentRouter,
   upvote: upvoteRouter,
   user: userRouter
 })
