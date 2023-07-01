e9b0d6aabc3bfddf730991606110c0184b3470b2
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-01 22:32:59 +0300

    Update footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className='w-full flex flex-col bg-dark-bg px-8 absolute bottom-0'>
+    <footer className='w-full flex flex-col bg-dark-bg px-8'>
       <div className="w-full h-20 flex items-center justify-center">
-        <p className="text-lg">
+        <p className="text-sm">
           Built by <Link
             href="https://www.github.com/emingbt"
             className='text-main-orange hover:text-light-orange hover:underline'
@@ -12,7 +12,7 @@ export default function Footer() {
         </p>
       </div>
       <div className='w-full h-0.5 bg-main-orange mb-6' />
-      <p className="w-full text-main-gray h-16 mb-6 text-sm">
+      <p className="w-full text-main-gray min-h-16 mb-6 text-xs">
         The assets comes from Satisfactory or from websites created and owned by Coffee Stain Studios, who hold the copyright of Satisfactory.
         <br />
         All trademarks and registered trademarks present in the image are proprietary to Coffee Stain Studios.
