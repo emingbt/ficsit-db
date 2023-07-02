c682065244e961e56c360b01ebdd4de8a8075f08
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-02 12:30:04 +0300

    Remove old desing and add first section in home page

--- frontend/app/home-page.tsx
+++ frontend/app/home-page.tsx
@@ -1,66 +1,29 @@
-'use-client'
-
-import Link from 'next/link'
 import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className='w-full h-full flex flex-col items-center'>
-      <section className='w-full flex flex-col items-center justify-center margin-0.5rem-0'>
-        <div className='w-[80%] h-[20vh] bg-error relative'>
-          <Image
-            src={'https://res.cloudinary.com/ficsitdb/image/upload/v1682541347/banner_ahwgdx.png'}
-            alt="Banner"
-            fill
-            priority
-          />
-        </div>
-        <div>
-          Improve your production line
-        </div>
-      </section>
-      {/* <StyledBannerSection>
-        <StyledBannerContainer>
-          <StyledBackgroundImage
-            src={'https://res.cloudinary.com/ficsitdb/image/upload/v1682541347/banner_ahwgdx.png'}
-            layout="fill"
-            objectFit='cover'
-            alt="Banner"
-            priority
-          />
-        </StyledBannerContainer>
-        <StyledBannerText>
-          Improve your production line
-        </StyledBannerText>
-      </StyledBannerSection>
-      <StyledNavSection>
-        <Link href="/items" legacyBehavior>
-          <StyledNavItem>
-            <StyledNavImage>
-              <StyledBackgroundImage
-                src={'https://res.cloudinary.com/ficsitdb/image/upload/v1682541340/items-bg_m55bbp.png'}
-                layout="fill"
-                objectFit='cover'
-                alt="Items"
-              />
-            </StyledNavImage>
-            <StyledNavText>Items</StyledNavText>
-          </StyledNavItem>
-        </Link>
-        <Link href="/buildings/production" legacyBehavior>
-          <StyledNavItem>
-            <StyledNavImage>
-              <StyledBackgroundImage
-                src={'https://res.cloudinary.com/ficsitdb/image/upload/v1682541330/buildings-bg_naskbu.png'}
-                layout="fill"
-                objectFit='cover'
-                alt="Buildings"
-              />
-            </StyledNavImage>
-            <StyledNavText>Buildings</StyledNavText>
-          </StyledNavItem>
-        </Link>
-      </StyledNavSection> */}
-    </main>
+    <div className='w-full h-full flex flex-col items-center p-5'>
+      <main className="w-full p-[10px] bg-dark-bg">
+        <section className="w-full h-[34rem] lg:h-screen flex flex-row items-center bg-main-bg">
+          <div className="w-full lg:w-1/2 flex flex-col p-4 font-bold font-secondary md:p-16">
+            <h1 className="text-4xl mb-2 lg:text-5xl">Hi Pioneer!</h1>
+            <p className="text-xs text-main-gray mb-8 lg:text-lg">Welcome to Ficsit DB pioneer guidance system.</p>
+            <div className="w-full xl:max-w-md flex flex-row items-center justify-between">
+              <button className="w-full max-w-[16rem] h-8 lg:h-10 bg-main-orange font-main font-normal hover:bg-light-bg">Sign up</button>
+              <p className="px-4 text-main-gray">or</p>
+              <button className="w-full max-w-[16rem] h-8 lg:h-10 bg-main-orange font-main font-normal hover:bg-light-bg">Login</button>
+            </div>
+          </div>
+          <div className="hidden lg:flex w-1/2 h-full bg-red-400 relative">
+            <Image
+              src="/images/landing-page.png"
+              alt="landing-page"
+              fill
+              className="object-cover object-center w-full h-full"
+            />
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
