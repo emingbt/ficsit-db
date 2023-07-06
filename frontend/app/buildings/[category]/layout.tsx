85312a2560dd8d619bd68d2ad56d2112fc1a7e06
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-06 15:42:46 +0300

    Create buildings page layout

---
+++ frontend/app/buildings/[category]/layout.tsx
@@ -0,0 +1,7 @@
+export default function BuildingsLayout({ children }) {
+  return (
+    <main className="w-full h-full flex flex-col items-center justify-cente p-2">
+      {children}
+    </main>
+  )
+}
