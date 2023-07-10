4c984255c3a4a09262a7bce0c5d1268cae127bd0
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-10 15:21:26 +0300

    Remove old building page route

--- frontend/app/buildings/[category]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function BuildingsLayout({ children }) {
-  return (
-    <main className="w-full h-full flex flex-col items-center justify-cente p-2">
-      {children}
-    </main>
-  )
-}
