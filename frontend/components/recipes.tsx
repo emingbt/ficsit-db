8b5049300b1662b682e5a252f386bb9c9b8a32bf
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-11 23:42:49 +0300

    Add keys to the divs in map

--- frontend/components/recipes.tsx
+++ frontend/components/recipes.tsx
@@ -7,12 +7,12 @@ export default function Recipes({ recipes, title }: { recipes: any[], title: str
         <h1>{title} - ({recipes.length})</h1>
       </div>
       <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 p-4 bg-light-bg">
-        {recipes.map((recipe) => (
-          <div className="w-full flex flex-col bg-dark-bg lg:hover:bg-main-bg cursor-pointer" key={recipe.id}>
+        {recipes.map((recipe, i) => (
+          <div className="w-full flex flex-col bg-dark-bg lg:hover:bg-main-bg cursor-pointer" key={i}>
             <div className="w-full flex flex-wrap items-center justify-center py-2">
-              {recipe.products.map((product) => (
+              {recipe.products.map((product, j) => (
                 <div className={`h-10 sm:h-16 aspect-square p-0.5 sm:p-1 m-1 sm:m-2 bg-light-bg
-                ${product.isFluid ? 'rounded-full' : 'rounded-sm sm:rounded-md'}`}>
+                ${product.isFluid ? 'rounded-full' : 'rounded-sm sm:rounded-md'}`} key={j}>
                   <div className='w-full h-full relative'>
                     <Image
                       src={product.imgUrl}
@@ -30,9 +30,9 @@ export default function Recipes({ recipes, title }: { recipes: any[], title: str
               <div className='w-full h-0.5 bg-main-orange' />
             </div>
             <div className="w-full flex flex-wrap items-center justify-center py-4">
-              {recipe.ingredients.map((ingredient) => (
+              {recipe.ingredients.map((ingredient, j) => (
                 <div className={`h-10 sm:h-16 aspect-square p-0.5 sm:p-1 m-1 sm:m-2 bg-light-bg
-                ${ingredient.isFluid ? 'rounded-full' : 'rounded-sm sm:rounded-md'}`}>
+                ${ingredient.isFluid ? 'rounded-full' : 'rounded-sm sm:rounded-md'}`} key={j}>
                   <div className='w-full h-full relative'>
                     <Image
                       src={ingredient.imgUrl}
