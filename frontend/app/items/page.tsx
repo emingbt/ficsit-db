d01508b1895c493f324b82557727d284d4773f00
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-19 20:34:35 +0300

    Update items page with new item card component

--- frontend/app/items/page.tsx
+++ frontend/app/items/page.tsx
@@ -1,5 +1,4 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import { ItemCard } from '../../components/itemCard'
 
 import { Item } from '../../interfaces'
 
@@ -9,27 +8,10 @@ export default async function Items() {
   const items = await result.json()
 
   return (
-    <section className='w-full grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-1 lg:gap-2 justify-center bg-light-bg p-1 lg: p-3'>
+    <section className='w-full grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-1 lg:gap-2 justify-center bg-light-bg p-2 lg:p-3'>
       {items.map((item: Item, i: number) => {
         return (
-          <Link href={`/items/${item.slug}`} key={i}>
-            <div className="flex flex-col items-center justify-center m-1 bg-main-bg lg:hover:bg-dark-bg pointer-cursor">
-              <div className='w-full aspect-square p-2'>
-                <div className='w-full min-h-[5rem] h-full relative'>
-                  <Image
-                    src={item.imgUrl}
-                    fill
-                    sizes='100%'
-                    alt={item.name}
-                    style={{ objectFit: 'contain' }}
-                  />
-                </div>
-              </div>
-              <div className='w-full h-16 flex items-center justify-center p-1 bg-dark-bg'>
-                <p className="text-center text-sm">{item.name}</p>
-              </div>
-            </div>
-          </Link>
+          <ItemCard item={item} key={i} />
         )
       })}
     </section>
