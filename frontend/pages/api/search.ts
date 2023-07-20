6373df7d5c497f647149b7c0312db026e93dff07
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-20 13:06:37 +0300

    Update the cypher query in search api

--- frontend/pages/api/search.ts
+++ frontend/pages/api/search.ts
@@ -5,10 +5,14 @@ export default async function Search(req: NextApiRequest, res: NextApiResponse) 
   const { name } = req.query
 
   const query = `
-  MATCH (i:Item)
-  MATCH (b:Building)
-  WHERE toLower(i.name) =~ $regex AND toLower(b.name) =~ $regex
-  RETURN COLLECT(DISTINCT {slug:i.slug, name:i.name, imgUrl:i.imgUrl}) AS items, COLLECT(DISTINCT {slug:b.slug, name:b.name, imgUrl:b.imgUrl}) AS buildings
+    OPTIONAL MATCH (i:Item)
+    WHERE toLower(i.name) =~ $regex
+    OPTIONAL MATCH (b:Building)
+    WHERE toLower(b.name) =~ $regex
+    WITH
+      COLLECT(DISTINCT CASE WHEN i IS NOT NULL THEN { slug: i.slug, name: i.name, imgUrl: i.imgUrl } ELSE NULL END) AS items,
+      COLLECT(DISTINCT CASE WHEN b IS NOT NULL THEN { slug: b.slug, name: b.name, imgUrl: b.imgUrl } ELSE NULL END) AS buildings
+    RETURN items, buildings
   `
 
   const params = {
