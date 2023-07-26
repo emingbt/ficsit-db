897f8be586cd833857183fdc4172fada0a0af8a8
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-26 18:14:04 +0300

    Update cypher query in the item api

--- frontend/pages/api/item.ts
+++ frontend/pages/api/item.ts
@@ -12,9 +12,7 @@ export default async function Items(req: NextApiRequest, res: NextApiResponse) {
     WITH r1, i, r2, COLLECT(DISTINCT {name: ingredient.slug, imgUrl: ingredient.imgUrl, isFluid: ingredient.isFluid, amount: u.amount}) AS r1Ingredients, COLLECT(DISTINCT {name: product.slug, imgUrl: product.imgUrl, isFluid: product.isFluid, amount: p.amount}) AS r1Products
     OPTIONAL MATCH (ingredient:Item)-[u:USED_IN]->(r2:ProductionRecipe)-[p:PRODUCES]->(product:Item)
     WITH r1, i ,r2, r1Ingredients, r1Products, COLLECT(DISTINCT {name: ingredient.slug, imgUrl: ingredient.imgUrl, isFluid: ingredient.isFluid, amount: u.amount}) AS r2Ingredients, COLLECT(DISTINCT {name: product.slug, imgUrl: product.imgUrl, isFluid: product.isFluid, amount: p.amount}) AS r2Products
-    OPTIONAL MATCH (r1)-[:PRODUCES_IN]->(b1:Building)
-    OPTIONAL MATCH (r2)-[:PRODUCES_IN]->(b2:Building)
-    WITH i, COLLECT(DISTINCT properties(r1{.*, products:r1Products, ingredients:r1Ingredients, building: {slug: b1.slug, imgUrl: b1.imgUrl}})) AS recipes, COLLECT(DISTINCT properties( r2{.*, products:r2Products, ingredients:r2Ingredients, building: {slug: b2.slug, imgUrl: b2.imgUrl}})) AS usagesAsIngredient
+    WITH i, COLLECT(DISTINCT properties(r1{.*, products:r1Products, ingredients:r1Ingredients})) AS recipes, COLLECT(DISTINCT properties( r2{.*, products:r2Products, ingredients:r2Ingredients})) AS usagesAsIngredient
     RETURN recipes, usagesAsIngredient, properties(i) AS item
   `
 
