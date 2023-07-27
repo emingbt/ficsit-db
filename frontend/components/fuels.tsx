abfbbedff6b573e00813da387a4327f66c466f90
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-27 18:33:06 +0300

    Update style in fuel component

--- frontend/components/fuels.tsx
+++ frontend/components/fuels.tsx
@@ -31,8 +31,8 @@ export default function Fuels({ fuels }: {
             <div className="w-full h-16 sm:h-24 flex flex-row items-center justify-center bg-main-bg p-1 sm:p-2">
               {fuel.items.map((item, j) => (
                 <>
-                  {item.type === 'PRODUCT_OF' && <div className='w-0.5 h-full sm:h-full bg-main-orange mx-1 sm:mx-2' />}
-                  <p className='text-xs sm:text-base'>{parseFloat(item.rate.toFixed(4))}x</p>
+                  {item.type === 'PRODUCT_OF' && <div className='w-0.5 h-full sm:h-full bg-main-orange ml-1 sm:ml-2' />}
+                  <p className='text-xs sm:text-base ml-2'>{parseFloat(item.rate.toFixed(4))}x</p>
                   <div className={`h-8 sm:h-14 aspect-square p-0.5 sm:p-1 m-1 sm:m-2 bg-light-bg
                   ${item.isFluid ? 'rounded-full' : 'rounded-sm sm:rounded-md'}`} key={j}>
                     <div className='w-full h-full relative'>
