6daa9bd4430352df1941dc48dc9e365c46f55e3b
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-07-27 19:13:31 +0300

    Add missing key props in fuels component

--- frontend/components/fuels.tsx
+++ frontend/components/fuels.tsx
@@ -27,10 +27,10 @@ export default function Fuels({ fuels }: {
       </div>
       <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4 bg-light-bg">
         {fuels.map((fuel, i) => (
-          <div>
+          <div key={i}>
             <div className="w-full h-16 sm:h-24 flex flex-row items-center justify-center bg-main-bg p-1 sm:p-2">
               {fuel.items.map((item, j) => (
-                <>
+                <div className='h-full flex flex-row items-center' key={i}>
                   {item.type === 'PRODUCT_OF' && <div className='w-0.5 h-full sm:h-full bg-main-orange ml-1 sm:ml-2' />}
                   <p className='text-xs sm:text-base ml-2'>{parseFloat(item.rate.toFixed(4))}x</p>
                   <div className={`h-8 sm:h-14 aspect-square p-0.5 sm:p-1 m-1 sm:m-2 bg-light-bg
@@ -45,7 +45,7 @@ export default function Fuels({ fuels }: {
                       />
                     </div>
                   </div>
-                </>
+                </div>
               ))}
             </div>
             <div className='w-full h-12 sm:h-12 flex items-center justify-center p-2 sm:p-4 bg-dark-bg text-sm sm:text-base'>
