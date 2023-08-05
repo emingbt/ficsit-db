b462d6cc19abadad981d52f4068d93899717292f
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-08-05 17:37:07 +0300

    Use new users router in app file

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { usersRouter } from './routers/users'
 
 const app = express()
 
@@ -8,6 +9,8 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/users', usersRouter)
 
 app.listen(8080, () => {
   console.log('Server listening on port 8080!')
