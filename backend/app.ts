e64d58788d5c855e0948851d8445844c746ade50
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-08-05 16:56:37 +0300

    Update app file with basic express code

--- backend/app.ts
+++ backend/app.ts
@@ -1,42 +1,14 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import express from 'express'
-import { expressHandler } from 'trpc-playground/handlers/express'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { appRouter } from './routers'
-import { createContext } from './utils/context'
-import path from 'path'
 
-const runApp = async () => {
-  const app = express()
+const app = express()
 
-  const trpcApiEndpoint = '/trpc'
-  const playgroundEndpoint = '/trpc-playground'
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-  app.set("views", path.join(__dirname, "views"))
-  app.set("view engine", "pug")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-  app.use(
-    trpcApiEndpoint,
-    trpcExpress.createExpressMiddleware({
-      router: appRouter,
-      createContext
-    }),
-  )
-
-  app.use(
-    playgroundEndpoint,
-    await expressHandler({
-      trpcApiEndpoint,
-      playgroundEndpoint,
-      router: appRouter,
-    }),
-  )
-
-  app.listen(8080, () => {
-    console.log('listening at http://localhost:8080')
-  })
-}
-
-runApp()
+app.listen(8080, () => {
+  console.log('Server listening on port 8080!')
+})
