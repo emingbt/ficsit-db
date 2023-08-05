e33414161fafd17c811046aeb34c91d457bdd8ea
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-08-05 17:39:44 +0300

    Add error handler to app file

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import { usersRouter } from './routers/users'
 
 const app = express()
@@ -10,8 +10,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Mount routers
 app.use('/users', usersRouter)
 
+// Error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err)
+  res.status(500).json({
+    message: 'Something went wrong'
+  })
+})
+
+// Start server
 app.listen(8080, () => {
   console.log('Server listening on port 8080!')
 })
