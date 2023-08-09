1fa066e07a83ca225b61a38e0c4323252f6024ca
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-08-09 12:31:14 +0300

    Update auth service

--- backend/services/auth.ts
+++ backend/services/auth.ts
@@ -1,85 +1,55 @@
 import bcrypt from 'bcrypt'
 import { prisma } from '../prisma'
-import { TRPCError } from '@trpc/server'
-import type { Context } from '../utils/trpc'
-import { generateToken } from '../utils/auth'
-import { sendEmail, EmailType } from '../utils/nodemailer'
+import createError from 'http-errors'
+import { sendEmail } from '../utils/nodemailer'
+import { generateToken, verifyToken } from '../utils/auth'
 
-const loginUser = async ({ input, ctx }: {
-  input: {
-    email: string,
-    password: string
-  },
-  ctx: Context
-}) => {
+const loginUser = async (
+  email: string,
+  password: string
+) => {
   // Check if user exists
   const user = await prisma.user.findUnique({
     where: {
-      email: input.email
+      email: email
     }
   })
 
   // If user does not exist, throw error
   if (!user) {
-    throw new TRPCError({
-      code: 'BAD_REQUEST',
-      message: 'User does not exist'
-    })
+    throw createError(404, 'User does not exist')
   }
 
   // Check if password is correct
   const isPasswordCorrect = await bcrypt.compare(
-    input.password,
+    password,
     user.password
   )
 
   // If password is incorrect, throw error
   if (!isPasswordCorrect) {
-    throw new TRPCError({
-      code: 'BAD_REQUEST',
-      message: 'Password is incorrect'
-    })
+    throw createError(400, 'Password is incorrect')
   }
 
   // Generate token
   const token = `Bearer ${generateToken(user.id)}`
 
-  // Set token in cookie
-  ctx.res.cookie('token', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30) // 1 month
-  })
-
-  // Remove password and return user
+  // Return user and token
   user.password = ''
-  return user
+  return { user, token }
 }
 
-const logoutUser = async ({ ctx }: {
-  ctx: Context
-}) => {
-  // Clear cookie
-  ctx.res.clearCookie('token')
-
-  return true
-}
-
-const forgotPassword = async (input: { email: string }) => {
+const forgotPassword = async (email: string) => {
   // Check if user exists
   const user = await prisma.user.findUnique({
     where: {
-      email: input.email
+      email: email
     }
   })
 
   // If user does not exist, throw error
   if (!user) {
-    throw new TRPCError({
-      code: 'NOT_FOUND',
-      message: 'User does not exist'
-    })
+    throw createError(404, 'User does not exist')
   }
 
   // Generate password reset token
@@ -97,29 +67,34 @@ const forgotPassword = async (input: { email: string }) => {
   return sentAddress
 }
 
-const resetPassword = async ({ input, ctx }: {
-  input: {
-    password: string,
-    token: string
-  },
-  ctx: Context
-}) => {
-  // If context does not have userId, throw error
-  // Actually, this should never happen, because
-  // there is a middleware that checks if token is valid
-  // But typescript does not know that
-  if (!ctx.userId) {
-    throw new TRPCError({
-      code: 'BAD_REQUEST',
-      message: 'Invalid token'
-    })
+const resetPassword = async (
+  password: string,
+  resetToken: string
+) => {
+  // Verify token
+  const userId = verifyToken(resetToken, "RESET_PASSWORD")
+
+  if (!userId) {
+    throw createError(401, 'Unauthorized')
+  }
+
+  // Check if user exists
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  })
+
+  // If user does not exist, throw error
+  if (!user) {
+    throw createError(404, 'User does not exist')
   }
 
   // Change password
-  const hashedPassword = await bcrypt.hash(input.password, 10)
+  const hashedPassword = await bcrypt.hash(password, 10)
   const updatedUser = await prisma.user.update({
     where: {
-      id: ctx.userId
+      id: userId
     },
     data: {
       password: hashedPassword
@@ -128,31 +103,19 @@ const resetPassword = async ({ input, ctx }: {
 
   // If user does not exist, throw error
   if (!updatedUser) {
-    throw new TRPCError({
-      code: 'NOT_FOUND',
-      message: 'User does not exist'
-    })
+    throw createError(500, 'User could not be updated')
   }
 
   // Generate token
   const token = `Bearer ${generateToken(updatedUser.id)}`
 
-  // Set token in cookie
-  ctx.res.cookie('token', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30) // 1 month
-  })
-
-  // Remove password and return user
+  // Return user and token
   updatedUser.password = ''
-  return updatedUser
+  return { updatedUser, token }
 }
 
 export {
   loginUser,
-  logoutUser,
   forgotPassword,
   resetPassword
 }
