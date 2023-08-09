1008b2c6b909c9584f0d1abfa738619f30165ad7
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-08-09 13:09:05 +0300

    Create signup function on auth service

--- backend/services/auth.ts
+++ backend/services/auth.ts
@@ -4,7 +4,7 @@ import createError from 'http-errors'
 import { sendEmail } from '../utils/nodemailer'
 import { generateToken, verifyToken } from '../utils/auth'
 
-const loginUser = async (
+const login = async (
   email: string,
   password: string
 ) => {
@@ -17,7 +17,7 @@ const loginUser = async (
 
   // If user does not exist, throw error
   if (!user) {
-    throw createError(404, 'User does not exist')
+    throw createError(404, 'There is no user with this email')
   }
 
   // Check if password is correct
@@ -39,6 +39,52 @@ const loginUser = async (
   return { user, token }
 }
 
+const signup = async (
+  username: string,
+  password: string,
+  email: string
+) => {
+  // Check if username is already taken
+  const userWithUsername = await prisma.user.findUnique({
+    where: {
+      username: username
+    }
+  })
+
+  if (userWithUsername) {
+    throw createError(409, 'Username is already taken')
+  }
+
+  // Check if email is already taken
+  const userWithEmail = await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  })
+
+  if (userWithEmail) {
+    throw createError(409, 'There is already an account with this email')
+  }
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      username: username,
+      password: hashedPassword,
+      email: email
+    }
+  })
+
+  // Generate token
+  const token = `Bearer ${generateToken(user.id)}`
+
+  // Return user and token
+  user.password = ''
+  return { user, token }
+}
+
 const forgotPassword = async (email: string) => {
   // Check if user exists
   const user = await prisma.user.findUnique({
@@ -49,7 +95,7 @@ const forgotPassword = async (email: string) => {
 
   // If user does not exist, throw error
   if (!user) {
-    throw createError(404, 'User does not exist')
+    throw createError(404, 'User not found')
   }
 
   // Generate password reset token
@@ -59,12 +105,13 @@ const forgotPassword = async (email: string) => {
   )
 
   // Send email with password reset link and return sent address
-  const sentAddress = sendEmail(
+  sendEmail(
     user.email,
     passwordResetToken,
     "RESET_PASSWORD"
   )
-  return sentAddress
+
+  return
 }
 
 const resetPassword = async (
@@ -115,7 +162,8 @@ const resetPassword = async (
 }
 
 export {
-  loginUser,
+  login,
+  signup,
   forgotPassword,
   resetPassword
 }
