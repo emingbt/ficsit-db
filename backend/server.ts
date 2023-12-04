a78fe749b8defaf8212a280c5327573172f07c7d
emingbt <mehmet.e.gbt@gmail.com>
2023-12-04 13:01:28 +0300

    Use new router in application

--- backend/server.ts
+++ backend/server.ts
@@ -1,25 +1,16 @@
-import { Application } from "https://deno.land/x/oak/mod.ts"
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts"
+
+import router from "./routers/index.ts"
 
 const app = new Application()
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 // Logger
 app.use(async (ctx, next) => {
   await next()
   const rt = ctx.response.headers.get("X-Response-Time")
   console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`)
-})
-
-// Timing
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  ctx.response.headers.set("X-Response-Time", `${ms}ms`)
-})
-
-// Hello World!
-app.use((ctx) => {
-  ctx.response.body = "Hello World!"
 })
 
 // Start server, console.log to confirm it's working
