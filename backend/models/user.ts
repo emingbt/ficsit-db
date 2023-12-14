ff2f137e1963fada11792e30924cc9da466cc1f3
emingbt <mehmet.e.gbt@gmail.com>
2023-12-14 22:22:25 +0300

    Update user model

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,15 +1,29 @@
 import { model, Schema } from 'npm:mongoose@^6.7'
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String
+  username: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    enum: ['user', 'admin'],
+    default: 'user',
+    type: String
+  }
 })
 
-userSchema.path('name').required(true, 'Name is required')
+userSchema.path('username').required(true, 'Username is required')
 userSchema.path('email').required(true, 'Email is required')
 userSchema.path('password').validate(function (password: string) {
   return password.length >= 6
 }, 'Password must be at least 6 characters')
 
-export default model('User', userSchema)
+export default model('userModel', userSchema)
