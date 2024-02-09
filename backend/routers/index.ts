fd5847421ec9f2b667faa6cc92480677fad0d865
emingbt <mehmet.e.gbt@gmail.com>
2024-02-09 17:19:13 +0300

    Remove unnecessary routes

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -1,16 +1,8 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts"
-import userModel from "../models/user.ts"
 import { createUser, getUserById, loginUser } from "../services/user.ts"
 import { createToken, verifyToken } from "../utils/jwt.ts"
 
 const router = new Router()
-
-router.get("/", async (ctx) => {
-  // get all users
-  const allUsers = await userModel.find({})
-  ctx.response.body = { users: allUsers }
-  ctx.response.status = 200
-})
 
 router.post("/signup", async (ctx) => {
   const { username, email, password } = await ctx.request.body().value
@@ -53,8 +45,6 @@ router.get("/logout", (ctx) => {
   ctx.response.status = 200
 })
 
-// Me;
-// Return the user from the token
 router.get("/me", async (ctx) => {
   const token = ctx.request.headers.get("Authorization")?.split(" ")[1]
 
@@ -76,52 +66,6 @@ router.get("/me", async (ctx) => {
     ctx.response.body = { message: error.message }
     ctx.response.status = 400
   }
-})
-
-router.get("/users", async (ctx) => {
-  const token = ctx.request.headers.get("Authorization")?.split(" ")[1]
-
-  if (!token) {
-    ctx.response.body = { message: "Not logged in" }
-    ctx.response.status = 400
-    return
-  }
-
-  try {
-    // Verify the token
-    const id = await verifyToken(token)
-
-    const user = await getUserById(id)
-
-    if (user.role !== "admin") {
-      ctx.response.body = { message: "Not authorized" }
-      ctx.response.status = 401
-      return
-    }
-
-    // Get all users
-    const users = await userModel.find({})
-
-    // Remove the password of the users before sending it
-    users.forEach((user) => {
-      user.password = ""
-    })
-
-    ctx.response.body = { users: users }
-    ctx.response.status = 200
-  } catch (error) {
-    ctx.response.body = { message: error.message }
-    ctx.response.status = 400
-  }
-})
-
-router.delete("/users/:id", async (ctx) => {
-  const id = ctx.params.id
-
-  const user = await userModel.deleteMany({ id: id })
-
-  ctx.response.body = { user: user }
-  ctx.response.status = 200
 })
 
 export default router
