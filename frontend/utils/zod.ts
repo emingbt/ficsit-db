4cba90020b67a80e4d7f436333c286fb61f9fe07
emingbt <mehmet.e.gbt@gmail.com>
2024-02-14 16:27:16 +0300

    Create zod file in utils folder

---
+++ frontend/utils/zod.ts
@@ -0,0 +1,12 @@
+import { object, string } from "zod"
+
+export const loginSchema = object({
+  email: string().email(),
+  password: string().min(8),
+})
+
+export const signupSchema = object({
+  email: string().email(),
+  username: string().min(3),
+  password: string().min(8),
+})
