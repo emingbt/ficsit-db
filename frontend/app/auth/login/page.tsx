b7c32688b574c2b39a26afd9e1bbcabf41b77294
emingbt <mehmet.e.gbt@gmail.com>
2024-07-16 20:03:39 +0300

    Move auth pages under the auth folder

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import LoginForm from '../../components/forms/loginForm'
+import LoginForm from '../../../components/forms/loginForm'
 
 export default function LoginPage() {
   return (
-    <main className=' w-full h-base xl:h-base-lg p-[10px] lg:p-4 bg-dark-bg'>
+    <main className='w-full h-base xl:h-base-lg p-[10px] lg:p-4 bg-dark-bg'>
       <div className='w-full h-full flex flex-row bg-main-bg'>
         <div className='w-full lg:w-1/2 xl:max-w-lg flex flex-col lg:justify-center p-6 lg:p-16 font-secondary'>
           <h1 className='text-2xl lg:text-4xl mb-4 lg:mb-1'>Login</h1>
