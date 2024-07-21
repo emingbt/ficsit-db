19eb938ada3e4190f3a1c8a10fc432a66ab5e1e3
emingbt <mehmet.e.gbt@gmail.com>
2024-07-21 15:33:49 +0300

    Create controllers and update routes

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,115 +1,13 @@
 import { Router } from "express"
-import { PrismaClient } from '@prisma/client'
-import bcrypt from "bcrypt"
 import validate from "../middleware/validate"
+import authenticate from "../middleware/authenticate"
 import { loginSchema, signUpSchema } from "../lib/authSchemas"
-import { createToken, verifyToken } from "../utils/jwt"
+import { login, signup, me } from "../controllers/auth"
 
 const authRouter = Router()
-const prisma = new PrismaClient()
 
-authRouter.post("/login", validate(loginSchema), async (req, res) => {
-  const { email, password } = req.body
-
-  const pioneer = await prisma.pioneer.findFirst({
-    where: {
-      email
-    }
-  })
-
-  if (!pioneer) {
-    return res.status(401).json({ error: "There isn't a pioneer with that email." })
-  }
-
-  const passwordMatch = bcrypt.compareSync(password, pioneer.password)
-  if (passwordMatch == false) {
-    return res.status(401).json({ error: "Password is incorrect." })
-  }
-
-  const token = createToken({
-    id: pioneer.id,
-    role: pioneer.role
-  })
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict"
-  })
-
-  res.json({
-    name: pioneer.name
-  })
-})
-
-authRouter.post("/signup", validate(signUpSchema), async (req, res) => {
-  const { name, email, password } = req.body
-
-  const pioneerExists = await prisma.pioneer.findFirst({
-    where: {
-      OR: [
-        { email },
-        { name }
-      ]
-    }
-  })
-
-  if (pioneerExists) {
-    return res.status(400).json({ error: "Email or name already exists." })
-  }
-
-  const pioneer = await prisma.pioneer.create({
-    data: {
-      name,
-      email,
-      password: bcrypt.hashSync(password, 10)
-    }
-  })
-
-  const token = createToken({
-    id: pioneer.id,
-    role: pioneer.role
-  })
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict"
-  })
-
-  res.json({
-    name: pioneer.name
-  })
-})
-
-authRouter.get("/me", async (req, res) => {
-  const token = req.headers.authorization?.split(" ")[1]
-
-  if (!token) {
-    return res.status(401).json({ error: "Not authenticated." })
-  }
-
-  try {
-    const decoded = verifyToken(token) as { id: number, role: string }
-    const pioneer = await prisma.pioneer.findUnique({
-      where: {
-        id: decoded.id
-      }
-    })
-
-    if (!pioneer) {
-      return res.status(401).json({ error: "Not authenticated." })
-    }
-
-    res.json({
-      name: pioneer.name,
-      email: pioneer.email,
-      createdAt: pioneer.createdAt
-    })
-  } catch (error) {
-    console.log(error)
-    res.status(401).json({ error: "Not authenticated." })
-  }
-})
+authRouter.post("/login", validate(loginSchema), login)
+authRouter.post("/signup", validate(signUpSchema), signup)
+authRouter.get("/me", authenticate, me)
 
 export default authRouter
