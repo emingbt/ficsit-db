1ab0534b14e4d13f5196817cfa370bb839cd7c38
emingbt <mehmet.e.gbt@gmail.com>
2024-07-21 10:23:05 +0300

    Import express-async-errors and implement global error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,17 @@
-import 'dotenv/config'
+import "dotenv/config"
 import express from "express"
+import "express-async-errors"
 import router from "./routes"
+import { errorHandler } from "./middleware/errors"
 
 const app = express()
 app.use(express.json())
 
 app.use("/", router)
 
-const PORT = process.env.PORT || 3000
+app.use(errorHandler)
 
+const PORT = process.env.PORT || 3000
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
