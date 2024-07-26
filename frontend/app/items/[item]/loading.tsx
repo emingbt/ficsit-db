91b0ba8e72c0148452c0e670776b5130d5059234
emingbt <mehmet.e.gbt@gmail.com>
2024-07-26 18:20:30 +0300

    Fix item page style that has been broken by deleting the parent layout

--- frontend/app/items/[item]/loading.tsx
+++ frontend/app/items/[item]/loading.tsx
@@ -1,69 +1,71 @@
 export default function Loading() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <section className="w-full flex flex-col md:flex-row items-center justify-center bg-main-bg text-xs sm:text-sm">
-        <div className="w-full md:w-3/5 h-24 sm:h-32 md:h-40 lg:h-48 flex flex-row items-center justify-center">
-          <div className="h-full aspect-square bg-light-bg p-2 md:p-4">
-            <div className="w-full h-full animate-pulse bg-main-bg rounded-md"
+    <main className="w-full h-full bg-dark-bg p-[10px] lg:p-4">
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <section className="w-full flex flex-col md:flex-row items-center justify-center bg-main-bg text-xs sm:text-sm">
+          <div className="w-full md:w-3/5 h-24 sm:h-32 md:h-40 lg:h-48 flex flex-row items-center justify-center">
+            <div className="h-full aspect-square bg-light-bg p-2 md:p-4">
+              <div className="w-full h-full animate-pulse bg-main-bg rounded-md"
+                style={{
+                  animationDelay: '0s',
+                  animationDuration: '1s',
+                }} />
+            </div>
+            <div className="w-full h-full flex flex-col">
+              <div className="w-full h-1/4 flex items-center justify-center bg-black xl:text-lg">
+                <span className="h-4 w-1/2 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.1s',
+                    animationDuration: '1s',
+                  }} />
+              </div>
+              <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
+                <span className="h-4 w-20 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.2s',
+                    animationDuration: '1s',
+                  }} />
+                <span className="h-4 w-6 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.3s',
+                    animationDuration: '1s',
+                  }} />
+              </div>
+              <div className="w-full h-1/4 flex flex-row items-center justify-between bg-dark-bg px-4">
+                <span className="h-4 w-20 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.4s',
+                    animationDuration: '1s',
+                  }} />
+                <span className="h-4 w-6 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.5s',
+                    animationDuration: '1s',
+                  }} />
+              </div>
+              <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
+                <span className="h-4 w-20 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.6s',
+                    animationDuration: '1s',
+                  }} />
+                <span className="h-4 w-6 animate-pulse bg-light-bg rounded-md"
+                  style={{
+                    animationDelay: '0.7s',
+                    animationDuration: '1s',
+                  }} />
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-2/5 h-auto md:h-full flex items-center justify-center p-6">
+            <span className="h-12 md:h-24 w-full animate-pulse bg-light-bg rounded-md"
               style={{
-                animationDelay: '0s',
+                animationDelay: '0.8s',
                 animationDuration: '1s',
               }} />
           </div>
-          <div className="w-full h-full flex flex-col">
-            <div className="w-full h-1/4 flex items-center justify-center bg-black xl:text-lg">
-              <span className="h-4 w-1/2 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.1s',
-                  animationDuration: '1s',
-                }} />
-            </div>
-            <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
-              <span className="h-4 w-20 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.2s',
-                  animationDuration: '1s',
-                }} />
-              <span className="h-4 w-6 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.3s',
-                  animationDuration: '1s',
-                }} />
-            </div>
-            <div className="w-full h-1/4 flex flex-row items-center justify-between bg-dark-bg px-4">
-              <span className="h-4 w-20 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.4s',
-                  animationDuration: '1s',
-                }} />
-              <span className="h-4 w-6 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.5s',
-                  animationDuration: '1s',
-                }} />
-            </div>
-            <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
-              <span className="h-4 w-20 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.6s',
-                  animationDuration: '1s',
-                }} />
-              <span className="h-4 w-6 animate-pulse bg-light-bg rounded-md"
-                style={{
-                  animationDelay: '0.7s',
-                  animationDuration: '1s',
-                }} />
-            </div>
-          </div>
-        </div>
-        <div className="w-full md:w-2/5 h-auto md:h-full flex items-center justify-center p-6">
-          <span className="h-12 md:h-24 w-full animate-pulse bg-light-bg rounded-md"
-            style={{
-              animationDelay: '0.8s',
-              animationDuration: '1s',
-            }} />
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </main>
   )
 }
--- frontend/app/items/[item]/page.tsx
+++ frontend/app/items/[item]/page.tsx
@@ -15,45 +15,47 @@ export default async function ItemPage({ params }: { params: { item: string } })
   const usagesAsIngredient: ProductionRecipe[] = data.usagesAsIngredient
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <section className="w-full flex flex-col md:flex-row items-center justify-center bg-main-bg text-xs sm:text-sm">
-        <div className="w-full md:w-3/5 h-24 sm:h-32 md:h-40 lg:h-48 flex flex-row items-center justify-center">
-          <div className="h-full aspect-square bg-light-bg p-2 md:p-4">
-            <div className="w-full h-full relative">
-              <Image
-                src={item.imgUrl}
-                fill
-                sizes="100%"
-                alt={item.name}
-                style={{ objectFit: 'contain' }}
-                priority
-              />
+    <main className="w-full h-full bg-dark-bg p-[10px] lg:p-4">
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <section className="w-full flex flex-col md:flex-row items-center justify-center bg-main-bg text-xs sm:text-sm">
+          <div className="w-full md:w-3/5 h-24 sm:h-32 md:h-40 lg:h-48 flex flex-row items-center justify-center">
+            <div className="h-full aspect-square bg-light-bg p-2 md:p-4">
+              <div className="w-full h-full relative">
+                <Image
+                  src={item.imgUrl}
+                  fill
+                  sizes="100%"
+                  alt={item.name}
+                  style={{ objectFit: 'contain' }}
+                  priority
+                />
+              </div>
+            </div>
+            <div className="w-full h-full flex flex-col">
+              <div className="w-full h-1/4 flex items-center justify-center bg-black xl:text-lg">
+                {item.name}
+              </div>
+              <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
+                <p>Stack Size:</p>
+                <p>{item.stackSize}</p>
+              </div>
+              <div className="w-full h-1/4 flex flex-row items-center justify-between bg-dark-bg px-4">
+                <p>Sink Value:</p>
+                <p>{item.sinkPoints}</p>
+              </div>
+              <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
+                <p>Radioactive:</p>
+                <p>{item.isRadioactive ? 'Yes' : 'No'}</p>
+              </div>
             </div>
           </div>
-          <div className="w-full h-full flex flex-col">
-            <div className="w-full h-1/4 flex items-center justify-center bg-black xl:text-lg">
-              {item.name}
-            </div>
-            <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
-              <p>Stack Size:</p>
-              <p>{item.stackSize}</p>
-            </div>
-            <div className="w-full h-1/4 flex flex-row items-center justify-between bg-dark-bg px-4">
-              <p>Sink Value:</p>
-              <p>{item.sinkPoints}</p>
-            </div>
-            <div className="w-full h-1/4 flex flex-row items-center justify-between px-4">
-              <p>Radioactive:</p>
-              <p>{item.isRadioactive ? 'Yes' : 'No'}</p>
-            </div>
+          <div className="w-full md:w-2/5 h-auto md:h-full flex items-center justify-center p-6">
+            <p>{item.description}</p>
           </div>
-        </div>
-        <div className="w-full md:w-2/5 h-auto md:h-full flex items-center justify-center p-6">
-          <p>{item.description}</p>
-        </div>
-      </section>
-      <Recipes recipes={recipes} title='Recipes' />
-      <Recipes recipes={usagesAsIngredient} title='Usages as Ingredient' />
-    </div>
+        </section>
+        <Recipes recipes={recipes} title='Recipes' />
+        <Recipes recipes={usagesAsIngredient} title='Usages as Ingredient' />
+      </div>
+    </main>
   )
 }
