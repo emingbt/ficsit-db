e34b0c82ce0eb6c5fd8d89954ab4761654e95750
emingbt <mehmet.e.gbt@gmail.com>
2024-07-27 10:31:18 +0300

    Fix warnings in searchInput style

--- frontend/components/searchInput.tsx
+++ frontend/components/searchInput.tsx
@@ -17,14 +17,14 @@ export default function SearchInput() {
         router.push(`/search?name=${searchValue}`)
       }}>
       <input
-        className='w-full h-full px-4 py-2 bg-dark-bg border border-main-orange rounded-full text-white outline-none bg-light-bg text-sm'
+        className='w-full h-full px-4 py-2 border border-main-orange rounded-full text-white outline-none bg-light-bg text-sm'
         type="text"
         placeholder='Search something...'
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
       />
       <button
-        className='w-10 h-10 flex items-center justify-center absolute right-0 cursor-pointer rounded-full border border-main-orange hover:bg-main-orange hover:bg-main-orange'
+        className='w-10 h-10 flex items-center justify-center absolute right-0 cursor-pointer rounded-full border border-main-orange hover:bg-main-orange'
         type='submit'
       >
         <SearchIcon />
