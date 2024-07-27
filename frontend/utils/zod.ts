ef04e5b39abe2c62f7b429ad447d991e03068065
emingbt <mehmet.e.gbt@gmail.com>
2024-07-27 22:46:45 +0300

    Update signup form schema

--- frontend/utils/zod.ts
+++ frontend/utils/zod.ts
@@ -6,9 +6,23 @@ export const LoginFormSchema = object({
 })
 
 export const SignupFormSchema = object({
-  email: string().email(),
-  name: string().min(3),
-  password: string().min(8),
+  name: string()
+    .min(3, { message: 'Name must be at least 3 characters long.' })
+    .max(64, { message: 'Name must be at most 64 characters long.' })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message: 'Name must contain only letters, numbers, and underscores.',
+    })
+    .trim(),
+  email: string().email({ message: 'Please enter a valid email.' }).trim(),
+  password: string()
+    .min(8, { message: 'Be at least 8 characters long' })
+    .max(64, { message: 'Be at most 64 characters long' })
+    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+    .regex(/[0-9]/, { message: 'Contain at least one number.' })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: 'Contain at least one special character.',
+    })
+    .trim(),
 })
 
 export const ForgotPasswordFormSchema = object({
