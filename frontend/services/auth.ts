0e021517c6d40791fc4d6f097120e93615fc813a
emingbt <mehmet.e.gbt@gmail.com>
2024-08-03 18:33:40 +0300

    Create auth service to get pioneer by email

---
+++ frontend/services/auth.ts
@@ -0,0 +1,25 @@
+import 'server-only'
+
+import db from '../utils/postgres'
+import { eq } from 'drizzle-orm'
+import { cache } from 'react'
+import { Pioneer } from '../drizzle/schema'
+
+export const getPioneerByEmail = cache(async (email: string) => {
+  try {
+    const data = await db.query.Pioneer.findFirst({
+      where: eq(Pioneer.email, email),
+
+      columns: {
+        name: true,
+        avatar: true,
+        color: true
+      }
+    })
+
+    return data
+  } catch (error) {
+    console.log(error)
+    return
+  }
+})
