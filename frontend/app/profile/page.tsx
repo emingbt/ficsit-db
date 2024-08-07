1cf365b4704e92a47bab29fbc426f26fab54e923
emingbt <mehmet.e.gbt@gmail.com>
2024-08-07 18:53:50 +0300

    Create Profile page

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,35 @@
+import { redirect } from "next/navigation"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { getPioneerByEmail } from "../../services/auth"
+import ProfileSection from "./profileSection"
+import AvatarSection from "./avatarSection"
+
+export default async function ProfilePage() {
+  const { getUser, isAuthenticated, refreshTokens } = getKindeServerSession()
+  const authenticated = await isAuthenticated()
+
+  if (!authenticated) {
+    redirect('/')
+  }
+
+  const user = await getUser()
+
+  if (!user || !user.email) {
+    redirect('/')
+  }
+  // Get the pioneer name, email, avatar, and color from the database
+  const pioneer = await getPioneerByEmail(user.email)
+
+  if (!pioneer) {
+    redirect('/create-pioneer')
+  }
+
+  return (
+    <main className="w-full h-full bg-dark-bg p-[10px] lg:p-4">
+      <div className="w-full h-full flex flex-col lg:flex-row items-center justify-center gap-5 bg-dark-bg">
+        <ProfileSection pioneer={pioneer} />
+        <AvatarSection pioneer={pioneer} />
+      </div>
+    </main>
+  )
+}
