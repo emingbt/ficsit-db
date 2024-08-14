98b3236174dea6fc22ad329226f9741db74b3943
emingbt <mehmet.e.gbt@gmail.com>
2024-08-14 15:51:51 +0300

    Create createBlueprint action

---
+++ app/create-blueprint/action.tsx
@@ -0,0 +1,114 @@
+'use server'
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { uploadFilesToCloudinary, uploadImagesToCloudinary } from "../../services/cloudinary"
+import { CrateBlueprintFormSchema } from "../../utils/zod"
+import { getPioneerByEmail } from "../../services/auth"
+import { redirect } from "next/navigation"
+import { createNewBlueprint, getBlueprintByTitle } from "../../services/blueprint"
+
+export default async function createBlueprint(state, formData: FormData) {
+  // 1. Validate form data
+  const validationResults = CrateBlueprintFormSchema.safeParse({
+    title: formData.get('title'),
+    description: formData.get('description'),
+    images: formData.getAll('images'),
+    files: formData.getAll('files'),
+    categories: formData.getAll('category')
+  })
+
+  if (!validationResults.success) {
+    return {
+      error: validationResults.error.flatten().fieldErrors,
+    }
+  }
+
+  const {
+    title,
+    description,
+    images,
+    files,
+    categories
+  } = validationResults.data
+
+  // 2. Check if the user is authenticated and get the pioneer name
+  const { isAuthenticated, getUser } = getKindeServerSession()
+
+  const authenticated = await isAuthenticated()
+
+  if (!authenticated) {
+    return {
+      error: {
+        submit: 'You must be logged in to create a blueprint.'
+      }
+    }
+  }
+
+  const user = await getUser()
+
+  if (!user || !user.email) {
+    return {
+      error: {
+        submit: 'User not found.'
+      }
+    }
+  }
+
+  // 3. Check if the user has a pioneer
+  const pioneer = await getPioneerByEmail(user.email)
+
+  if (!pioneer || !pioneer.name) {
+    redirect('/create-pioneer')
+  }
+
+  // 4. Check if the user has another blueprint with the same title
+  const blueprintExist = await getBlueprintByTitle(title)
+
+  if (blueprintExist) {
+    return {
+      error: {
+        submit: 'A blueprint with the same title already exists.'
+      }
+    }
+  }
+
+  // 5. Upload images and files to cloudinary, then create the blueprint
+  try {
+    const imageUrls = await uploadImagesToCloudinary(images, pioneer.name, title)
+    const fileUrls = await uploadFilesToCloudinary(files, pioneer.name, title)
+
+    const blueprint = {
+      title,
+      description,
+      images: imageUrls,
+      files: fileUrls,
+      categories,
+      pioneerName: pioneer.name
+    }
+
+    // 6. Create the blueprint
+    const newBlueprint = await createNewBlueprint(blueprint)
+
+    if (!newBlueprint) {
+      return {
+        error: {
+          submit: 'Failed to create the blueprint. Try again later.'
+        }
+      }
+    }
+
+    return {
+      success: {
+        data: newBlueprint.title,
+        submit: 'Blueprint created successfully.'
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      error: {
+        submit: 'Failed to create the blueprint. Try again later.'
+      }
+    }
+  }
+}
