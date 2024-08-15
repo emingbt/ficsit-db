c3c3aa41801a80d058bff024c0f750c8d679e0b7
emingbt <mehmet.e.gbt@gmail.com>
2024-08-15 15:47:07 +0300

    Create getPageCountAndBlueprintsByPage in blueprint service

--- services/blueprint.ts
+++ services/blueprint.ts
@@ -1,7 +1,7 @@
 import 'server-only'
 
 import db from '../utils/postgres'
-import { eq } from 'drizzle-orm'
+import { count, desc, eq } from 'drizzle-orm'
 import { Blueprint } from '../drizzle/schema'
 
 export const getBlueprintByTitle = async (title: string) => {
@@ -44,5 +44,33 @@ export const createNewBlueprint = async (blueprint: Blueprint) => {
   } catch (error) {
     console.log(error)
     throw new Error('Failed to create the blueprint.')
+  }
+}
+
+export const getPageCountAndBlueprintsByPage = async (page: number) => {
+  const blueprintsPerPage = 30
+  const offset = (page - 1) * blueprintsPerPage
+
+  if (page < 1) {
+    return {
+      pageCount: 0,
+      blueprints: []
+    }
+  }
+
+  try {
+    const blueprints = await db.select().from(Blueprint)
+      .orderBy(desc(Blueprint.createdAt))
+      .limit(blueprintsPerPage)
+      .offset(offset)
+
+    const totalBlueprints = (await db.select({ value: count() }).from(Blueprint))[0].value
+
+    const pageCount = Math.ceil(totalBlueprints / blueprintsPerPage)
+
+    return { pageCount, blueprints }
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to get the blueprints.')
   }
 }
