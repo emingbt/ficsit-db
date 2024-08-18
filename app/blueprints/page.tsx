b190aca048d9845c822ea394a7e68b742cf251af
emingbt <mehmet.e.gbt@gmail.com>
2024-08-18 19:37:46 +0300

    Update header and section gap in blueprints page

--- app/blueprints/page.tsx
+++ app/blueprints/page.tsx
@@ -8,10 +8,10 @@ export default async function BlueprintsPage({ searchParams }: { searchParams: {
 
   return (
     <main className="w-full h-full bg-dark-bg p-[10px] lg:p-4">
-      <div className="w-full h-10 sm:h-12 flex items-center bg-logo-blue pl-4">
-        <h1 className="text-xl sm:text-2xl font-medium text-white">Blueprints</h1>
+      <div className="w-full h-10 sm:h-12 flex items-center bg-main-bg pl-4">
+        <h1 className="text-xl sm:text-2xl font-medium text-logo-blue">Blueprints</h1>
       </div>
-      <section className='w-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 lg:gap-4 justify-center bg-light-bg p-2 lg:p-4 mb-[10px] lg:mb-4'>
+      <section className='w-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 lg:gap-3 justify-center bg-light-bg p-2 lg:p-3 mb-[10px] lg:mb-4'>
         {
           blueprints?.length > 0 ?
             blueprints.map((blueprint, i: number) => {
