d80a0a37c7f9dfc99dd8d84864c2b8036b485ca9
emingbt <mehmet.e.gbt@gmail.com>
2024-08-20 18:12:35 +0300

    Add edit blueprints section to the profile page and update Blueprint card component

--- components/blueprintCard.tsx
+++ components/blueprintCard.tsx
@@ -9,9 +9,9 @@ type Card = {
   averageRating: number
 }
 
-export default function BlueprintCard({ blueprint }: { blueprint: Card }) {
+export default function BlueprintCard({ blueprint, isEdit }: { blueprint: Card, isEdit?: boolean }) {
   return (
-    <Link href={`/blueprints/${blueprint.id}`}>
+    <Link href={`/${isEdit ? "edit-blueprints" : "blueprints"}/${blueprint.id}`}>
       <div className="flex flex-col items-center justify-center bg-main-bg lg:hover:bg-dark-bg cursor-pointer">
         <div className="w-full aspect-video p-1 sm:p-2">
           <div className='w-full h-full relative'>
