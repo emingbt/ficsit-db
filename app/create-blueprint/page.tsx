3dbb508b41cd57080ab4209a61dda8a7bdb38819
emingbt <mehmet.e.gbt@gmail.com>
2024-08-22 13:48:40 +0300

    Update create blueprint page style

--- app/create-blueprint/page.tsx
+++ app/create-blueprint/page.tsx
@@ -14,7 +14,7 @@ export default async function CreatePioneerPage() {
   return (
     <main className='w-full min-h-base xl:min-h-base-lg p-[10px] lg:p-4 bg-dark-bg'>
       <div className='w-full min-h-content lg:min-h-content-lg xl:min-h-content-xl flex flex-row bg-main-bg'>
-        <div className='w-full lg:w-1/2 flex flex-col lg:justify-center p-6 lg:p-16 font-secondary'>
+        <div className='w-full lg:w-1/2 lg:min-w-[480px] flex flex-col lg:justify-center p-6 lg:p-10 xl:p-16 font-secondary'>
           <h1 className='text-2xl lg:text-4xl mb-4 lg:mb-1'>Create a new blueprint</h1>
           <p className=' text-xs lg:text-base mb-4 text-main-gray'>Share your ideas with other pioneers.</p>
           <CreateBlueprintForm />
