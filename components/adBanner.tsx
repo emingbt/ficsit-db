8ae51046c403310234270958fec2f3d5d6af8d90
emingbt <mehmet.e.gbt@gmail.com>
2024-10-19 13:29:13 +0300

    Create ad banner component and test it in blueprint page

---
+++ components/adBanner.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useEffect } from "react"
+
+type AdBannerTypes = {
+  dataAdSlot: string
+  dataAdFormat?: string
+  dataFullWidthResponsive?: boolean
+}
+
+const AdBanner = ({
+  dataAdSlot,
+  dataAdFormat = "auto",
+  dataFullWidthResponsive = true,
+}: AdBannerTypes) => {
+  useEffect(() => {
+    try {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+        {}
+      )
+    } catch (error: any) {
+      console.log(error.message)
+    }
+  }, [])
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-1772997678438254"
+      data-ad-slot={dataAdSlot}
+      data-ad-format={dataAdFormat}
+      data-full-width-responsive={dataFullWidthResponsive.toString()}
+    ></ins>
+  )
+}
+
+export default AdBanner
