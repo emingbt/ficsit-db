3a0ef37a2c6f3b85fbc2d4873eb20646171be49f
emingbt <mehmet.e.gbt@gmail.com>
2025-03-13 01:16:30 +0300

    Update create and edit blueprint pages with new Main component

--- app/create-blueprint/page.tsx
+++ app/create-blueprint/page.tsx
@@ -1,7 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import Image from "next/image"
 import { redirect } from "next/navigation"
 import CreateBlueprintForm from "./form"
+import Main from "../../components/Main"
 
 export default async function CreatePioneerPage() {
   const { isAuthenticated } = getKindeServerSession()
@@ -12,24 +12,10 @@ export default async function CreatePioneerPage() {
   }
 
   return (
-    <main className='w-full min-h-base xl:min-h-base-lg p-[10px] lg:p-4 bg-dark-bg'>
-      <div className='w-full min-h-content lg:min-h-content-lg xl:min-h-content-xl flex flex-row bg-main-bg'>
-        <div className='w-full lg:w-1/2 lg:min-w-[480px] flex flex-col lg:justify-center p-6 lg:p-10 xl:p-16 font-secondary'>
-          <h1 className='text-2xl lg:text-4xl mb-4 lg:mb-1'>Create a new blueprint</h1>
-          <p className=' text-xs lg:text-base mb-4 text-main-gray'>Share your ideas with other pioneers.</p>
-          <CreateBlueprintForm />
-        </div>
-        <div className="hidden w-1/2 lg:flex relative">
-          <Image
-            src="/images/create-blueprint-banner.webp"
-            alt="blueprint-page"
-            fill
-            className="object-cover object-left w-full h-full"
-            sizes='100%'
-            priority
-          />
-        </div>
-      </div>
-    </main>
+    <Main classname="p-8" image='create-blueprint-banner'>
+      <h1 className='text-2xl lg:text-4xl mb-4 lg:mb-1'>Create a new blueprint</h1>
+      <p className=' text-xs lg:text-base mb-4 text-main-gray'>Share your ideas with other pioneers.</p>
+      <CreateBlueprintForm />
+    </Main>
   )
 }
