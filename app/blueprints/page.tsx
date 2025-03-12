5eab4f82adc51f63846321c8c6227e3b18d3dfca
emingbt <mehmet.e.gbt@gmail.com>
2025-03-13 00:42:23 +0300

    Update blueprint page and blueprint card component for Main component

--- app/blueprints/page.tsx
+++ app/blueprints/page.tsx
@@ -1,17 +1,19 @@
 import { getPageCountAndBlueprintsByPage } from "../../services/blueprint"
 import BlueprintCard from "../../components/blueprintCard"
 import Pagination from "../../components/pagination"
+import Main from "../../components/Main"
 
 export default async function BlueprintsPage({ searchParams }: { searchParams: { page: string } | undefined }) {
   const page = searchParams?.page ? parseInt(searchParams?.page) : 1
   const { pageCount, blueprints } = await getPageCountAndBlueprintsByPage(page)
+  // blueprints.push(...blueprints)
 
   return (
-    <main className="w-full h-full bg-dark-bg p-[10px] lg:p-4">
+    <Main>
       <div className="w-full h-10 sm:h-12 flex items-center bg-main-bg pl-4">
         <h1 className="text-xl sm:text-2xl font-medium text-logo-blue">Blueprints</h1>
       </div>
-      <section className='w-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 lg:gap-3 justify-center bg-light-bg p-2 lg:p-3 mb-[10px] lg:mb-4'>
+      <section className='w-full min-h-[calc(100%-96px)] grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 lg:gap-3 justify-center bg-light-bg p-2 lg:p-3'>
         {
           blueprints?.length > 0 ?
             blueprints.map((blueprint, i: number) => {
@@ -23,6 +25,6 @@ export default async function BlueprintsPage({ searchParams }: { searchParams: {
         }
       </section>
       <Pagination path='/blueprints' currentPage={page} pageCount={pageCount} />
-    </main>
+    </Main>
   )
 }
--- components/blueprintCard.tsx
+++ components/blueprintCard.tsx
@@ -11,40 +11,42 @@ type Card = {
 
 export default function BlueprintCard({ blueprint, isEdit }: { blueprint: Card, isEdit?: boolean }) {
   return (
-    <Link href={`/${isEdit ? "edit-blueprint" : "blueprints"}/${blueprint.id}`}>
-      <div className="flex flex-col items-center justify-center bg-main-bg lg:hover:bg-dark-bg cursor-pointer">
-        <div className="w-full aspect-video p-1 sm:p-2">
-          <div className='w-full h-full relative'>
-            <Image
-              src={blueprint.images[0]}
-              fill
-              sizes='100%'
-              alt={blueprint.title}
-              style={{ objectFit: 'cover' }}
-              unoptimized
-            />
+    <div>
+      <Link href={`/${isEdit ? "edit-blueprint" : "blueprints"}/${blueprint.id}`}>
+        <div className="flex flex-col items-center justify-center bg-main-bg lg:hover:bg-dark-bg cursor-pointer">
+          <div className="w-full aspect-video p-1 sm:p-2">
+            <div className='w-full h-full relative'>
+              <Image
+                src={blueprint.images[0]}
+                fill
+                sizes='100%'
+                alt={blueprint.title}
+                style={{ objectFit: 'cover' }}
+                unoptimized
+              />
+            </div>
           </div>
+          <div className='w-full flex flex-col p-2 sm:p-4'>
+            <div className="w-full h-10 sm:h-12 flex items-center justify-center mb-2 sm:mb-4 font-medium text-xl overflow-hidden">
+              <p className="text-sm sm:text-lg text-center font-medium line-clamp-2">
+                {blueprint.title}
+              </p>
+            </div>
+            <div className="w-full h-3 flex flex-row justify-center items-center gap-1 mb-2 sm:mb-1">
+              {
+                Array.from({ length: 5 }).map((_, index) => (
+                  <StarIcon
+                    key={index}
+                    //@ts-ignore
+                    className={`w-4 h-4 text-logo-blue ${index < blueprint.averageRating ? 'fill-main-orange' : 'fill-light-bg'}`}
+                  />
+                ))
+              }
+            </div>
+          </div>
+          <div className='w-full h-1 bg-logo-blue' />
         </div>
-        <div className='w-full flex flex-col p-2 sm:p-4'>
-          <div className="w-full h-10 sm:h-12 flex items-center justify-center mb-2 sm:mb-4 font-medium text-xl overflow-hidden">
-            <p className="text-sm sm:text-lg text-center font-medium line-clamp-2">
-              {blueprint.title}
-            </p>
-          </div>
-          <div className="w-full h-3 flex flex-row justify-center items-center gap-1 mb-2 sm:mb-1">
-            {
-              Array.from({ length: 5 }).map((_, index) => (
-                <StarIcon
-                  key={index}
-                  //@ts-ignore
-                  className={`w-4 h-4 text-logo-blue ${index < blueprint.averageRating ? 'fill-main-orange' : 'fill-light-bg'}`}
-                />
-              ))
-            }
-          </div>
-        </div>
-        <div className='w-full h-1 bg-logo-blue' />
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
