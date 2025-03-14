1110fbb551fe33f9370a1fbf567b1e416e8e559f
emingbt <mehmet.e.gbt@gmail.com>
2025-03-14 21:04:47 +0300

    Update pioneer page with Main component

--- app/pioneers/[pioneerName]/page.tsx
+++ app/pioneers/[pioneerName]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import BlueprintCard from "../../../components/blueprintCard"
 import { getAllBlueprintsByPioneer } from "../../../services/blueprint"
 import { getPioneerByName } from "../../../services/pioneer"
+import Main from "../../../components/Main"
 
 export default async function PioneerPage({ params }: { params: { pioneerName: string } }) {
   const pioneerName = params.pioneerName
@@ -23,7 +24,7 @@ export default async function PioneerPage({ params }: { params: { pioneerName: s
   const blueprints = await getAllBlueprintsByPioneer(pioneerName)
 
   return (
-    <main className="w-full h-full bg-dark-bg p-[10px] lg:p-4">
+    <Main classname="flex flex-col bg-dark-bg">
       <div className="w-full h-16 bg-black flex sm:hidden items-center justify-center text-lg font-semibold">
         {pioneer.name}
       </div>
@@ -61,10 +62,10 @@ export default async function PioneerPage({ params }: { params: { pioneerName: s
       <div className="w-full h-[50px] sm:h-[90px] flex lg:hidden items-center justify-center bg-black mb-[10px]">
         Ad
       </div>
-      <div className="w-full h-10 sm:h-12 flex items-center bg-main-bg pl-4">
+      <div className="w-full h-8 sm:h-14 flex items-center bg-main-bg pl-4">
         <h1 className="text-lg sm:text-xl font-medium">Blueprints</h1>
       </div>
-      <section className='w-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 lg:gap-3 justify-center bg-light-bg p-2 lg:p-3 mb-[10px] lg:mb-4'>
+      <section className='w-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 lg:gap-3 justify-center bg-light-bg p-2 lg:p-3'>
         {
           blueprints?.length > 0 ?
             blueprints.map((blueprint, i: number) => {
@@ -72,9 +73,11 @@ export default async function PioneerPage({ params }: { params: { pioneerName: s
                 <BlueprintCard key={i} blueprint={blueprint} />
               )
             }) :
-            <p className='text-2xl'>No blueprints found</p>
+            <div className="col-span-2 sm:col-span-2 lg:col-span-3 xl:col-span-5 w-full h-full flex items-center justify-center">
+              <p className='text-xl my-16'>No blueprints found</p>
+            </div>
         }
       </section>
-    </main>
+    </Main>
   )
 }
