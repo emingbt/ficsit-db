8c57950c9f98abc8611e6fe99830c80c38826b06
emingbt <mehmet.e.gbt@gmail.com>
2025-03-18 18:43:29 +0300

    Update ad banner component

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,38 +1,46 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-type AdBannerTypes = {
+type AdBannerProps = {
+  classname: string
   dataAdSlot: string
   dataAdFormat?: string
   dataFullWidthResponsive?: boolean
 }
 
-const AdBanner = ({
-  dataAdSlot,
-  dataAdFormat = "auto",
-  dataFullWidthResponsive = false,
-}: AdBannerTypes) => {
+export default function AdBanner({ classname, dataAdSlot, dataFullWidthResponsive = false, dataAdFormat = "auto", }: AdBannerProps) {
+  const [adsLoaded, setAdsLoaded] = useState(false)
+
   useEffect(() => {
-    try {
-      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
-        {}
-      )
-    } catch (error: any) {
-      console.log(error.message)
+    if (typeof window !== "undefined") {
+      // ✅ Ensure adsbygoogle is correctly initialized as an array
+      window.adsbygoogle = window.adsbygoogle || { push: () => { } }
+
+      if (!adsLoaded) {
+        try {
+          window.adsbygoogle.push({})
+          setAdsLoaded(true)
+        } catch (error: any) {
+          console.error("AdSense Error:", error.message)
+        }
+      }
     }
-  }, [])
+  }, [adsLoaded])
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block", width: "100%", height: "100%" }}
-      data-ad-client="ca-pub-1772997678438254"
-      data-ad-slot={dataAdSlot}
-      data-ad-format={dataAdFormat}
-      data-full-width-responsive={dataFullWidthResponsive.toString()}
-    ></ins>
+    <div className={`${classname} relative bg-main-bg text-gray-600 text-lg lg:text-xl font-semibold`}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", width: "100%", height: "100%" }}
+        data-ad-client="ca-pub-1772997678438254"
+        data-ad-slot={dataAdSlot}
+        data-ad-format={dataAdFormat}
+        data-full-width-responsive={dataFullWidthResponsive.toString()}
+      ></ins>
+      <div className="absolute w-full h-full flex items-center justify-center">
+        Ad
+      </div>
+    </div>
   )
 }
-
-export default AdBanner
