542dd9f0329ea07e23180dbd07179353d769f47d
emingbt <mehmet.e.gbt@gmail.com>
2025-03-24 17:11:40 +0300

    Update blueprints page

--- app/blueprints/(main)/page.tsx
+++ app/blueprints/(main)/page.tsx
@@ -3,14 +3,30 @@ import Pagination from "../../../components/Pagination"
 import BlueprintContainer from "../../../components/BlueprintContainer"
 import { getPageCountAndBlueprintsByPage } from "../../../services/blueprint"
 
-export default async function BlueprintsPage({ searchParams }: { searchParams: { page: string } | undefined }) {
+import type { Blueprint } from "../../../drizzle/schema"
+
+export default async function BlueprintsPage({ searchParams }: {
+  searchParams: {
+    page: string,
+    category: string,
+    sort: string
+  } | undefined
+}) {
   const page = searchParams?.page ? parseInt(searchParams?.page) : 1
-  const { pageCount, blueprints } = await getPageCountAndBlueprintsByPage(page)
+  const category = searchParams?.category as Blueprint["categories"][number] | undefined
+  const sort = searchParams?.sort
+
+  const { pageCount, blueprints } = await getPageCountAndBlueprintsByPage(page, category, sort)
 
   return (
     <Main classname="flex flex-col items-stretch">
-      <BlueprintContainer blueprints={blueprints} title="Blueprints" />
-      <Pagination path='/blueprints' currentPage={page} pageCount={pageCount} />
+      <BlueprintContainer blueprints={blueprints} title="Blueprints" filter={{ category, sort }} />
+      <Pagination
+        path="/blueprints"
+        currentPage={page}
+        pageCount={pageCount}
+        filterPath={`${category ? `&category=${category}` : ""}${sort ? `&sort=${sort}` : ""}`}
+      />
     </Main>
   )
 }
