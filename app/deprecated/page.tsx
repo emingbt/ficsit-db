8e73b1893e3626d978991bbd2afbda46778f0ed4
emingbt <mehmet.e.gbt@gmail.com>
2025-03-29 17:17:26 +0300

    Update deprecated page

--- app/deprecated/page.tsx
+++ app/deprecated/page.tsx
@@ -3,29 +3,15 @@ import Main from "../../components/Main"
 export default function Deprecated() {
   return (
     <Main classname="p-2 lg:p-4 flex flex-col items-center justify-center sm:text-lg text-left font-secondary">
-      <div>
-        <h1 className="text-lg sm:text-2xl font-bold">
-          Yes, I know it is <span className="text-error">deprecated</span>
-        </h1>
-        <br />
-        <p>
-          Satisfactory 1.0 is released 🎉 and there are lots of new things.
-        </p>
-        <p>
-          But I&apos;m still at the university and in the last year.
-        </p>
-        <p>
-          Right now, I&apos;m working on my graduation project, so I don&apos;t have too much free time.
-        </p>
-        <p>
-          I&apos;ll try to update the website as soon as possible.
-        </p>
-        <br />
-        <p className="text-main-orange">Thank you for your understanding.</p>
-        <p>
-          - emingbt
-        </p>
+      <div className="p-4 sm:p-8 lg:p-16">
+        I am aware that some elements of this website are deprecated.<br /><br />
+        With the release of Satisfactory 1.0, many exciting new features have been introduced.<br />
+        However, as I am currently in my final year of university and focusing on my graduation project, my available time is quite limited.<br />
+        I appreciate your patience and understanding, and I will do my best to update the website as soon as possible.<br /><br />
+        Thank you for your support.<br />
+        — emingbt
       </div>
     </Main>
   )
 }
+
