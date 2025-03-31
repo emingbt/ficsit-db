ac39768120d580ecde675dc972169ccb0f2f1bae
emingbt <mehmet.e.gbt@gmail.com>
2025-03-31 13:43:58 +0300

    Update uploaded file and image size to be less than 1MB

--- utils/zod.ts
+++ utils/zod.ts
@@ -107,13 +107,13 @@ export const CreateBlueprintFormSchema = object({
   images: any()
     .refine((images) => images?.length > 0, "At least 1 image is required.")
     .refine((images) => images?.length <= 3, "At most 3 images are allowed.")
-    .refine((files) => files?.every((file: File) => file.size < 5000000), {
-      message: `File size must be less than 5MB.`,
+    .refine((files) => files?.every((file: File) => file.size < 1000000), {
+      message: `File size must be less than 1MB.`,
     }),
   files: any()
     .refine((files) => files?.length == 2, "Both .sbp and .sbpcfg files are required.")
-    .refine((files) => files?.every((file: File) => file.size < 5000000), {
-      message: `File size must be less than 5MB.`,
+    .refine((files) => files?.every((file: File) => file.size < 1000000), {
+      message: `File size must be less than 1MB.`,
     }),
   categories: array(CategoriesEnum)
     .min(1, { message: 'At least one category has to selected.' })
