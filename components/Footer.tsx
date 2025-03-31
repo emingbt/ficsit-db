1800e4991149e58bbac45885489331f8c88bd2f9
emingbt <mehmet.e.gbt@gmail.com>
2025-03-31 13:45:44 +0300

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link"
+import Image from "next/image"
+import { Star } from "lucide-react"
 
 export default function Footer() {
   return (
     <footer className='w-full flex flex-col bg-dark-bg px-8 font-secondary'>
-      <div className="w-full h-20 flex items-center justify-center">
-        <p className="text-sm xl:text-base">
-          Built by <Link
-            href="https://www.github.com/emingbt"
-            className='text-main-orange hover:text-light-orange hover:underline'
-          >emingbt</Link>
-        </p>
+      <div className="w-full flex flex-col sm:flex-row items-start sm:items-center justify-between py-4">
+        <div className="flex flex-col sm:flex-row text-gray-600 mb-4 sm:mb-0 items-start sm:items-center">
+          <p className=" font-logo">FiCS<span className="-ml-[2px]">I</span><span className="-ml-[5px]">T</span> DB</p>
+          {/* <p className="font-logo ml-2">v1.0</p> */}
+          <p className="font-medium sm:ml-2">Built by pioneers for pioneers.</p>
+        </div>
+        <Link
+          href="https://www.github.com/emingbt/ficsit-db"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+          className='group flex items-center text-sm xl:text-base p-2 rounded-full bg-main-bg'>
+          <Image src="/icons/github-mark-white.svg" alt="GitHub" width={24} height={24} className="fill-gray-600" />
+          <p className="ml-4 text-gray-600 font-medium group-hover:text-gray-400">Give a star!</p>
+          <Star className="text-gray-600 fill-gray-600 ml-2 group-hover:text-main-orange group-hover:fill-main-orange" size={16} />
+        </Link>
       </div>
       <div className='w-full h-0.5 bg-main-orange mb-6' />
       <p className="w-full text-main-gray min-h-16 mb-6 text-xs">
