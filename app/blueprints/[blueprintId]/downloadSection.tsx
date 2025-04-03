02d353a6843656f1f900b19e693f55da03b08c7a
emingbt <mehmet.e.gbt@gmail.com>
2025-04-03 14:51:07 +0300

    Update download section style

--- app/blueprints/[blueprintId]/downloadSection.tsx
+++ app/blueprints/[blueprintId]/downloadSection.tsx
@@ -9,7 +9,7 @@ export default function DownloadSection({ blueprintId, files, pioneerName }: { b
   }
 
   return (
-    <div className="w-full lg:w-1/3 min-h-64 sm:min-h-48 flex flex-grow flex-col sm:flex-row lg:flex-col 2xl:flex-row items-center justify-evenly font-bold pb-6 sm:pb-0 lg:pb-8 xl:p-0 bg-foo">
+    <div className="w-full lg:w-1/3 min-h-64 sm:min-h-48 flex flex-grow flex-col sm:flex-row lg:flex-col 2xl:flex-row items-center justify-evenly font-bold sm:pb-0 xl:p-0 bg-foo">
       <div className="w-36 lg:w-48 h-16 bg-logo-blue hover:text-logo-blue hover:bg-main-bg rounded">
         <Link
           href={files[0]}
