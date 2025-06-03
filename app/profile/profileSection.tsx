e774b74db4ad42f313a0ff385a418cf9b0a616fc
emingbt <mehmet.e.gbt@gmail.com>
2025-06-03 18:42:06 +0300

    Remove logout link from the profile page

--- app/profile/profileSection.tsx
+++ app/profile/profileSection.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Pioneer } from "../../interfaces"
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs"
 
 export default function ProfileSection({ pioneer }: { pioneer: Pioneer }) {
   const year = pioneer.createdAt.getFullYear()
@@ -18,11 +17,6 @@ export default function ProfileSection({ pioneer }: { pioneer: Pioneer }) {
           <p>Email: {pioneer.email}</p>
           <p>Created At: {createdAt}</p>
         </div>
-        <LogoutLink
-          className='w-1/2 lg:w-48 h-10 flex items-center justify-center bg-red-600 cursor-pointer hover:bg-red-500'
-        >
-          <p className='user-select-none'>Logout</p>
-        </LogoutLink>
       </div>
     </section>
   )
