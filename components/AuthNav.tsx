f17951b9044689c9cddec066f2409adb1e8431b1
emingbt <mehmet.e.gbt@gmail.com>
2025-06-03 18:43:04 +0300

    Update the authentication logic inside the auth nav component

--- components/AuthNav.tsx
+++ components/AuthNav.tsx
@@ -15,28 +15,25 @@ export default function AuthNavigation() {
   const dropdownRef = useRef<HTMLDivElement>(null)
   const pathname = usePathname()
 
-  let pioneer: {
-    name: string
-    avatar: string
-    color: string
-  } | undefined
+  // Fetch the pioneer data from the store as pioneer
+  const { name, avatar, color, setAvatar, setName, setColor } = usePioneerStore((state) => state)
+  const pioneer = { name, avatar, color }
 
-  // fetch the pioneer data from the store as pioneer
-  const { name, avatar, color, setAvatar, setColor, setName } = usePioneerStore((state) => state)
-  pioneer = { name, avatar, color }
+  // If the store is empty, fetch the pioneer data from the access token
+  useEffect(() => {
+    if (!isAuthenticated) return
 
-  // if the pioneer data is not available, get it from the access token
-  if (!name || !avatar || !color) {
-    const accessToken = getAccessToken()
-    pioneer = getPropertiesFromAccessToken(accessToken || undefined)
+    if ((!name || !avatar || !color)) {
+      const accessToken = getAccessToken()
+      const pioneerFromToken = getPropertiesFromAccessToken(accessToken || undefined)
 
-    if (pioneer) {
-      // set the pioneer data in the store
-      setName(pioneer.name)
-      setAvatar(pioneer.avatar)
-      setColor(pioneer.color)
+      if (!pioneerFromToken) return
+
+      setName(pioneerFromToken.name)
+      setAvatar(pioneerFromToken.avatar)
+      setColor(pioneerFromToken.color)
     }
-  }
+  }, [isAuthenticated])
 
   // Close dropdown when clicking outside
   useEffect(() => {
@@ -60,7 +57,7 @@ export default function AuthNavigation() {
   return (
     <>
       {
-        pioneer ? (
+        checkIfPioneerExists(pioneer) ? (
           <div className='h-full'>
             <Link
               href='/profile'
@@ -69,7 +66,7 @@ export default function AuthNavigation() {
               <p className='text-white'>{pioneer.name}</p>
               <Image
                 src={`/images/avatars/${pioneer.avatar}.png`}
-                alt='Avatar'
+                alt={`${pioneer.name}'s avatar`}
                 width={40}
                 height={40}
                 className={`rounded-full bg-avatar-${pioneer.color}`}
@@ -82,7 +79,7 @@ export default function AuthNavigation() {
             <div ref={dropdownRef} className='h-full hidden lg:flex items-center relative gap-2 ml-6'>
               <Image
                 src={`/images/avatars/${pioneer.avatar}.png`}
-                alt='Avatar'
+                alt={`${pioneer.name}'s avatar`}
                 width={40}
                 height={40}
                 className={`rounded-full bg-avatar-${pioneer.color} cursor-pointer outline ${isDropdownOpen && `outline-2  outline-offset-1 outline-avatar-${pioneer.color}`}`}
@@ -109,8 +106,7 @@ export default function AuthNavigation() {
                 <p className='h-full text-center user-select-none lg:cursor-pointer'>Login</p>
               </LoginLink>
             </> :
-            <LogoutLink
-              className='w-32 lg:w-16 lg:ml-6 lg:hover:text-main-orange'>
+            <LogoutLink className='w-32 lg:w-16 lg:ml-6 lg:hover:text-main-orange'>
               <p className='h-full text-center user-select-none lg:cursor-pointer'>Logout</p>
             </LogoutLink>
         )
@@ -120,14 +116,22 @@ export default function AuthNavigation() {
 }
 
 function AvatarDropdown() {
+  const { resetStore } = usePioneerStore((state) => state)
+
   return (
     <div className='absolute top-0 right-0 mt-16 w-24 bg-dark-bg rounded-b-md shadow-lg z-20'>
-      <Link href='/profile' className='block px-4 py-2 text-gray-300 hover:bg-main-bg'>
+      <Link href='/profile' className='block px-4 py-2 text-gray-300 hover:bg-main-bg' >
         Profile
       </Link>
-      <LogoutLink className='block px-4 py-2 text-red-500 hover:bg-main-bg'>
+      <LogoutLink className='block px-4 py-2 text-red-500 hover:bg-main-bg'
+        onClick={() => resetStore()}
+      >
         Logout
       </LogoutLink>
     </div>
   )
+}
+
+function checkIfPioneerExists(pioneer: { name: string, avatar: string, color: string }) {
+  return !!pioneer && !!pioneer.name && !!pioneer.avatar && !!pioneer.color
 }
