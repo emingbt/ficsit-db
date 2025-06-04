732b62fd98d47d1a9f840b8bc44596dc94b080f7
emingbt <mehmet.e.gbt@gmail.com>
2025-06-04 16:34:26 +0300

    Update main component style

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
 
-export default function Main({ children, classname, dontFill = false, image, imagePosition }: { children: React.ReactNode, classname?: string, dontFill?: boolean, image?: string, imagePosition?: string }) {
+export default function Main({ children, classname = "", dontFill = false, image, imagePosition }: { children: React.ReactNode, classname?: string, dontFill?: boolean, image?: string, imagePosition?: string }) {
   const isDivided: boolean = !!image
 
   return (
     <div className={`w-full flex ${dontFill ? "" : " min-h-[calc(100vh-104px)] xl:min-h-[calc(100vh-128px)]"} items-stretch border-8 lg:border-[1rem] border-dark-bg`}>
-      <main className={`w-full ${isDivided ? "lg:w-1/2" : ""} ${classname}`}>
+      <main className={`w-full${isDivided ? " lg:w-1/2" : ""}${classname ? ` ${classname}` : ""}`}>
         {children}
       </main>
       {/* Image that fills the remaining space (flex-1) */}
