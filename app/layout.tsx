52cd726f61d7bdccb04d34911fb43e559ddf048c
emingbt <mehmet.e.gbt@gmail.com>
2025-06-05 21:11:16 +0300

    Add maze script to the html head

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,29 @@ export default function RootLayout({ children }: {
             gtag('config', 'G-JSTXP7VT57');
           `}
         </Script>
+        <Script>
+          {`
+            (function (m, a, z, e) {
+              var s, t;
+                try {
+                  t = m.sessionStorage.getItem('maze-us');
+                } catch (err) { }
+
+                if (!t) {
+                  t = new Date().getTime();
+                  try {
+                    m.sessionStorage.setItem('maze-us', t);
+                  } catch (err) { }
+                }
+
+              s = a.createElement('script');
+              s.src = z + '?apiKey=' + e;
+              s.async = true;
+              a.getElementsByTagName('head')[0].appendChild(s);
+              m.mazeUniversalSnippetApiKey = e;
+            })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'c2d814e1-2557-4b20-97de-7a49e3c45f02');
+          `}
+        </Script>
         <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
