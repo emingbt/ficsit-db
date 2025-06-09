5f9bf7b0eee0a079fafa00022525f86eb4952d7d
emingbt <mehmet.e.gbt@gmail.com>
2025-06-09 19:52:41 +0300

    Create social links schema in zod utils file

--- utils/zod.ts
+++ utils/zod.ts
@@ -1,4 +1,4 @@
-import { array, any, enum as enum_, object, string, union } from "zod"
+import { array, any, enum as enum_, object, string } from "zod"
 
 export const LoginFormSchema = object({
   email: string().email({ message: 'Please enter a valid email.' }).trim(),
@@ -114,7 +114,7 @@ export const CreateBlueprintFormSchema = object({
     .max(3, { message: 'At most 3 categories can be selected.' }),
   videoUrl: string().trim().transform((val) => (val === "" ? undefined : val))
     .refine((url) => !url || url.startsWith("https://"), {
-      message: "URL must start with https://",
+      message: "Must start with https://",
     })
     .refine(
       (url) =>
@@ -139,7 +139,7 @@ export const UpdateBlueprintFormSchema = object({
     .max(3, { message: 'At most 3 categories can be selected.' }),
   videoUrl: string().trim().transform((val) => (val === "" ? undefined : val))
     .refine((url) => !url || url.startsWith("https://"), {
-      message: "URL must start with https://",
+      message: "Must start with https://",
     })
     .refine(
       (url) =>
@@ -151,4 +151,35 @@ export const UpdateBlueprintFormSchema = object({
       }
     )
     .optional()
+})
+
+const httpsRegex = /^https:\/\//
+const platformPatterns = {
+  youtube: /^(https:\/\/)(www\.)?youtube\.com/,
+  twitch: /^(https:\/\/)(www\.)?twitch\.tv/,
+  kick: /^(https:\/\/)(www\.)?kick\.com/,
+  discord: /^(https:\/\/)(www\.)?discord\.gg/,
+  reddit: /^(https:\/\/)(www\.)?reddit\.com/,
+  github: /^(https:\/\/)(www\.)?github\.com/,
+}
+
+export const SocialLinksSchema = object({
+  youtube: string().trim().url("Must be valid URL")
+    .regex(httpsRegex, "Must start with https://")
+    .regex(platformPatterns.youtube, "Must be valid YouTube URL").optional(),
+  twitch: string().trim().url("Invalid URL")
+    .regex(httpsRegex, "Must start with https://")
+    .regex(platformPatterns.twitch, "Must be valid Twitch URL").optional(),
+  kick: string().url("Invalid URL")
+    .regex(httpsRegex, "Must start with https://")
+    .regex(platformPatterns.kick, "Must be valid Kick URL").optional(),
+  discord: string().url("Invalid URL")
+    .regex(httpsRegex, "Must start with https://")
+    .regex(platformPatterns.discord, "Must be valid Discord URL").optional(),
+  reddit: string().url("Invalid URL")
+    .regex(httpsRegex, "Must start with https://")
+    .regex(platformPatterns.reddit, "Must be valid Reddit URL").optional(),
+  github: string().url("Invalid URL")
+    .regex(httpsRegex, "Must start with https://")
+    .regex(platformPatterns.github, "Must be valid GitHub URL").optional(),
 })
