47a0b30d46627289e0331dc6939df785afb300c0
emingbt <mehmet.e.gbt@gmail.com>
2025-06-13 21:58:46 +0300

    Connect development database if it is not production

--- utils/postgres.ts
+++ utils/postgres.ts
@@ -3,7 +3,11 @@ import { drizzle } from 'drizzle-orm/neon-http'
 
 import * as schema from '../drizzle/schema'
 
-const connectionString = process.env.POSTGRESQL_DATABASE_URL
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = isProduction
+  ? process.env.POSTGRESQL_DATABASE_URL
+  : process.env.POSTGRESQL_DEV_DATABASE_URL
+
 
 if (!connectionString) {
   throw new Error('POSTGRESQL_DATABASE_URL environment variable is required')
