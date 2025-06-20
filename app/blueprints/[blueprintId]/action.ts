8f32768458e2b8a7800fea13503d04f11dad9991
emingbt <mehmet.e.gbt@gmail.com>
2025-06-20 22:27:10 +0300

    Check the blueprint, when rating the blueprint

--- app/blueprints/[blueprintId]/action.ts
+++ app/blueprints/[blueprintId]/action.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { incrementBlueprintDownloads } from "../../../services/blueprint"
 import { createOrUpdateBlueprintRating, getBlueprintRating } from "../../../services/rating"
@@ -10,6 +11,26 @@ import { CreateCommentSchema, UpdateCommentSchema } from "../../../utils/zod"
 
 export const rateBlueprint = async (blueprintId: number, pioneerName: string, rating: number) => {
   try {
+    const { getUser, isAuthenticated } = getKindeServerSession()
+    if (!isAuthenticated()) {
+      throw new Error('Unauthorized')
+    }
+
+    const user = await getUser()
+    if (!user || !user.email) {
+      throw new Error('Unauthorized')
+    }
+
+    const pioneer = await getPioneerByEmail(user.email)
+    if (!pioneer) {
+      // Redirect to create pioneer page if the pioneer does not exist
+      redirect('/create-pioneer')
+    }
+
+    if (pioneer.name !== pioneerName) {
+      throw new Error('Unauthorized.')
+    }
+
     const averageRating = await createOrUpdateBlueprintRating(blueprintId, pioneerName, rating)
 
     revalidatePath(`/blueprints/${blueprintId}`)
