ea5eb015bedd44298db17f2bb29827950790c0b7
emingbt <mehmet.e.gbt@gmail.com>
2025-07-12 21:39:38 +0300

    Update landing page image and auth link style

--- components/AuthLinks.tsx
+++ components/AuthLinks.tsx
@@ -6,13 +6,13 @@ export default function AuthLinks() {
   return (
     <div className="flex flex-row gap-4 text-sm lg:text-base font-secondary">
       <LoginLink
-        className="w-1/2 max-w-[16rem] h-8 lg:h-10 flex items-center justify-center bg-light-bg lg:hover:bg-main-orange text-light-orange lg:hover:text-white rounded-sm"
+        className="w-1/2 max-w-[16rem] h-8 lg:h-10 flex items-center justify-center bg-main-orange lg:hover:bg-light-bg text-white lg:hover:text-light-orange rounded-sm"
         postLoginRedirectURL='/api/auth'
       >
         Login
       </LoginLink>
       <RegisterLink
-        className="w-1/2 max-w-[16rem] h-8 lg:h-10 flex items-center justify-center bg-light-bg lg:hover:bg-main-orange text-light-orange lg:hover:text-white rounded-sm"
+        className="w-1/2 max-w-[16rem] h-8 lg:h-10 flex items-center justify-center bg-main-orange lg:hover:bg-light-bg text-white lg:hover:text-light-orange rounded-sm"
         postLoginRedirectURL='/api/auth'
       >
         Sign up
