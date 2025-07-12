1951d98b00d499965a36554b2f9d518c3aa514a3
emingbt <mehmet.e.gbt@gmail.com>
2025-07-12 20:18:41 +0300

    Create an additional file extension check condition on file input component

--- components/FileInput.tsx
+++ components/FileInput.tsx
@@ -21,6 +21,12 @@ export default function FileInput({ fileError }: { fileError: string }) {
         return
       }
 
+      const ext = file.name.split('.').pop()?.toLowerCase()
+      if ((index === 0 && ext !== "sbp") || (index === 1 && ext !== "sbpcfg")) {
+        alert(`Please upload a ${index === 0 ? ".sbp" : ".sbpcfg"} file`)
+        return
+      }
+
       newFiles[index] = file
       setFiles(newFiles)
     }
